f292df31bc0b8155dcbe03177470f2b8acf44512
Jim Pivarski <jpivarski@gmail.com>
2020-05-15 16:18:13 -0500

    Include XRootDVectorReadSource in the tests and get it to raise an exception (rather than hang) on a mistyped filename.

--- uproot4/source/xrootd.py
+++ uproot4/source/xrootd.py
@@ -12,12 +12,11 @@ import uproot4.source.chunk
 import uproot4.source.futures
 
 
-def get_pyxrootd():
+def import_pyxrootd():
     os.environ["XRD_RUNFORKHANDLER"] = "1"  # set multiprocessing flag
     try:
         import pyxrootd.client
-
-        return pyxrootd
+        import XRootD.client
 
     except ImportError:
         raise ImportError(
@@ -28,6 +27,9 @@ def get_pyxrootd():
 (or download from http://xrootd.org/dload.html and manually compile with """
             """cmake; setting PYTHONPATH and LD_LIBRARY_PATH appropriately)."""
         )
+
+    else:
+        return pyxrootd, XRootD
 
 
 def get_server_config(file_path):
@@ -43,23 +45,112 @@ def get_server_config(file_path):
         readv_ior_max (int): The maximum number of bytes that can be requested
             per **element** in a vector read
     """
-    from XRootD.client import FileSystem, URL
-    from XRootD.client.flags import QueryCode
+    pyxrootd, XRootD = import_pyxrootd()
 
-    url = URL(file_path)
-    eos = FileSystem("{}://{}/".format(url.protocol, url.hostid))
+    url = XRootD.client.URL(file_path)
+    fs = XRootD.client.FileSystem("{0}://{1}/".format(url.protocol, url.hostid))
 
-    status, readv_iov_max = eos.query(QueryCode.CONFIG, "readv_iov_max")
+    status, readv_iov_max = fs.query(
+        XRootD.client.flags.QueryCode.CONFIG, "readv_iov_max"
+    )
     if not status.ok:
-        raise NotImplementedError(status)
+        raise OSError(status.message)
     readv_iov_max = int(readv_iov_max)
 
-    status, readv_ior_max = eos.query(QueryCode.CONFIG, "readv_ior_max")
+    status, readv_ior_max = fs.query(
+        XRootD.client.flags.QueryCode.CONFIG, "readv_ior_max"
+    )
     if not status.ok:
-        raise NotImplementedError(status)
+        raise OSError(status.message)
     readv_ior_max = int(readv_ior_max)
 
     return readv_iov_max, readv_ior_max
+
+
+class XRootDResource(uproot4.source.chunk.Resource):
+    """
+    Resource wrapping a pyxrootd.File.
+    """
+
+    __slots__ = ["_file_path", "_file"]
+
+    def __init__(self, file_path, timeout):
+        """
+        Args:
+            file_path (str): URL starting with "root://".
+            timeout (int): Number of seconds (loosely interpreted by XRootD)
+                before giving up on a remote file.
+        """
+
+        pyxrootd, XRootD = import_pyxrootd()
+        self._file_path = file_path
+        self._timeout = timeout
+        self._file = pyxrootd.client.File()
+
+        status, dummy = self._file.open(
+            self._file_path, timeout=(0 if timeout is None else timeout)
+        )
+
+        if status.get("error", None):
+            self._file.close(timeout=(0 if self._timeout is None else self._timeout))
+            raise OSError(status["message"])
+
+    @property
+    def file_path(self):
+        """
+        URL starting with "root://".
+        """
+        return self._file_path
+
+    @property
+    def timeout(self):
+        """
+        Number of seconds (loosely interpreted by XRootD) before giving up on a
+        remote file.
+        """
+        return self._timeout
+
+    @property
+    def file(self):
+        """
+        The pyxrootd.File handle.
+        """
+        return self._file
+
+    def __enter__(self):
+        """
+        Does nothing and returns self.
+        """
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        """
+        Closes the pyxrootd.File.
+        """
+        self._file.close(timeout=(0 if self._timeout is None else self._timeout))
+
+    def get(self, start, stop):
+        """
+        Args:
+            start (int): Starting byte position to extract (inclusive, global
+                in Source).
+            stop (int): Stopping byte position to extract (exclusive, global
+                in Source).
+
+        Returns a subinterval of the `raw_data` using global coordinates as a
+        NumPy array with dtype uint8.
+
+        The start and stop must be `Chunk.start <= start <= stop <= Chunk.stop`.
+
+        Calling this function blocks until `raw_data` is filled.
+        """
+        status, data = self._file.read(
+            start, stop - start, timeout=(0 if self._timeout is None else self._timeout)
+        )
+        if status.get("error", None):
+            self._file.close(timeout=(0 if self._timeout is None else self._timeout))
+            raise OSError(status["message"])
+        return data
 
 
 class XRootDVectorReadSource(uproot4.source.chunk.Source):
@@ -81,10 +172,14 @@ class XRootDVectorReadSource(uproot4.source.chunk.Source):
         """
         self._file_path = file_path
         self._timeout = timeout
+
+        # important: construct this first because it raises an error for nonexistent hosts
+        self._resource = XRootDResource(file_path, timeout)
+
+        # this comes after because it HANGS for nonexistent hosts
         self._max_num_elements, self._max_element_size = get_server_config(file_path)
         if max_num_elements:
             self._max_num_elements = min(self._max_num_elements, max_num_elements)
-        self._resource = XRootDResource(file_path, timeout)
 
     def __enter__(self):
         """
@@ -143,88 +238,6 @@ class XRootDVectorReadSource(uproot4.source.chunk.Source):
                 raise OSError("XRootD error: " + status["message"])
 
         return chunks
-
-
-class XRootDResource(uproot4.source.chunk.Resource):
-    """
-    Resource wrapping a pyxrootd.File.
-    """
-
-    __slots__ = ["_file_path", "_file"]
-
-    def __init__(self, file_path, timeout):
-        """
-        Args:
-            file_path (str): URL starting with "root://".
-            timeout (int): Number of seconds (loosely interpreted by XRootD)
-                before giving up on a remote file.
-        """
-        pyxrootd = get_pyxrootd()
-        self._file_path = file_path
-        self._timeout = timeout
-
-        self._file = pyxrootd.client.File()
-        status, dummy = self._file.open(
-            self._file_path, timeout=(0 if timeout is None else timeout)
-        )
-        if status.get("error", None):
-            raise OSError(status["message"])
-
-    @property
-    def file_path(self):
-        """
-        URL starting with "root://".
-        """
-        return self._file_path
-
-    @property
-    def timeout(self):
-        """
-        Number of seconds (loosely interpreted by XRootD) before giving up on a
-        remote file.
-        """
-        return self._timeout
-
-    @property
-    def file(self):
-        """
-        The pyxrootd.File handle.
-        """
-        return self._file
-
-    def __enter__(self):
-        """
-        Does nothing and returns self.
-        """
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        """
-        Closes the pyxrootd.File.
-        """
-        self._file.close(timeout=(0 if self._timeout is None else self._timeout))
-
-    def get(self, start, stop):
-        """
-        Args:
-            start (int): Starting byte position to extract (inclusive, global
-                in Source).
-            stop (int): Stopping byte position to extract (exclusive, global
-                in Source).
-
-        Returns a subinterval of the `raw_data` using global coordinates as a
-        NumPy array with dtype uint8.
-
-        The start and stop must be `Chunk.start <= start <= stop <= Chunk.stop`.
-
-        Calling this function blocks until `raw_data` is filled.
-        """
-        status, data = self._file.read(
-            start, stop - start, timeout=(0 if self._timeout is None else self._timeout)
-        )
-        if status.get("error", None):
-            raise OSError(status["message"])
-        return data
 
 
 class XRootDSource(uproot4.source.chunk.MultiThreadedSource):
