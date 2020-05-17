4365974c530d8945e55e99601b5a76c1a37cc9f1
Jim Pivarski <jpivarski@gmail.com>
2020-05-17 10:33:36 -0500

    Serialization model is a little clearer.

--- uproot4/model.py
+++ uproot4/model.py
@@ -93,11 +93,18 @@ def classname_pretty(obj):
 
 class Model(object):
     @classmethod
-    def read(cls, cursor, file, parent):
+    def read(cls, cursor, chunk, file, parent, encoded_classname=None):
         self = cls.__new__(cls)
         self._cursor = cursor.copy(link_refs=True)
         self._file = file
         self._parent = parent
+        if type(cls).__name__.startswith("ROOT_"):
+            self._encoded_classname = type(cls).__name__
+        elif encoded_classname is not None:
+            self._encoded_classname = encoded_classname
+        else:
+            self._encoded_classname = "ROOT__3f3f3f_"
+        self._num_bytes = None
         self._members = {}
         self._bases = []
         return self
@@ -113,6 +120,22 @@ class Model(object):
     @property
     def parent(self):
         return self._parent
+
+    @property
+    def encoded_classname(self):
+        return self._encoded_classname
+
+    @property
+    def classname(self):
+        return classname_decode(self._encoded_classname)[0]
+
+    @property
+    def class_version(self):
+        return classname_decode(self._encoded_classname)[1]
+
+    @property
+    def num_bytes(self):
+        return self._num_bytes
 
     @property
     def members(self):
@@ -142,22 +165,19 @@ _numbytes_version_1 = struct.Struct(">IH")
 _numbytes_version_2 = struct.Struct(">H")
 
 
-def _numbytes_version(cursor, source):
-    num_bytes, version = cursor.fields(source, _numbytes_version_1, move=False)
+def _numbytes_version(cursor, chunk):
+    num_bytes, version = cursor.fields(chunk, _numbytes_version_1, move=False)
     num_bytes = numpy.int64(num_bytes)
 
     if num_bytes & uproot4._const.kByteCountMask:
         num_bytes = int(num_bytes & ~uproot4._const.kByteCountMask)
-        moved_cursor = cursor.copy(link_refs=True)
-        moved_cursor.skip(_numbytes_version_1.size)
+        cursor.skip(_numbytes_version_1.size)
 
     else:
         num_bytes = None
-        version = cursor.field(source, _numbytes_version_2, move=False)
-        moved_cursor = cursor.copy(link_refs=True)
-        moved_cursor.skip(_numbytes_version_2.size)
+        version = cursor.field(chunk, _numbytes_version_2)
 
-    return moved_cursor, num_bytes, version
+    return num_bytes, version
 
 
 def _numbytes_check(start_cursor, stop_cursor, num_bytes, classname, file_path):
@@ -165,7 +185,7 @@ def _numbytes_check(start_cursor, stop_cursor, num_bytes, classname, file_path):
         observed = stop_cursor.displacement(start_cursor)
         if observed != num_bytes:
             raise ValueError(
-                """ROOT object of type {0} has {1} bytes; expected {2}
+                """instance of ROOT class {0} has {1} bytes; expected {2}
 in file {4}""".format(
                     classname, observed, num_bytes, file_path
                 )
@@ -174,39 +194,39 @@ in file {4}""".format(
 
 class UnknownClass(Model):
     @classmethod
-    def read(cls, cursor, file, parent):
-        return Model.read(cursor, file, parent)
+    def read(cls, cursor, chunk, file, parent, encoded_classname=None):
+        self = Model.read(
+            cursor, chunk, file, parent, encoded_classname=encoded_classname
+        )
+
+        num_bytes, version = _numbytes_version(cursor, chunk)
+
+        if num_bytes is not None:
+            self._num_bytes = num_bytes
+            cursor.skip(num_bytes)
+            return self
+
+        else:
+            raise ValueError(
+                """Unknown class {1} that cannot be skipped because its """
+                """number of bytes is unknown.
+""".format(
+                    classname_pretty(self._encoded_classname), list(cls._class_versions)
+                )
+            )
 
     def __repr__(self):
         return "<Unknown {0}>".format(self.classname)
 
-    @property
-    def encoded_classname(self):
-        return self._encoded_classname
-
-    @property
-    def classname(self):
-        return classname_decode(self._classname)[0]
-
 
 class UnknownClassVersion(Model):
-    def read_members(self, cursor):
-        out = self._cursor.copy(link_refs=False)
-        out.skip(self._num_bytes)
-        return out
+    def read_bases_members(self, cursor):
+        cursor.skip(self._num_bytes)
 
     def __repr__(self):
         return "<Unknown {0} version {1}>".format(
             self.classname, self._instance_version
         )
-
-    @property
-    def encoded_classname(self):
-        return self._encoded_classname
-
-    @property
-    def classname(self):
-        return classname_decode(self._classname)[0]
 
     @property
     def instance_version(self):
@@ -219,21 +239,20 @@ class UnknownClassVersion(Model):
 
 class ModelVersions(object):
     @classmethod
-    def read(cls, cursor, file, parent):
-        moved_cursor, num_bytes, version = _numbytes_version(cursor, file.source)
+    def read(cls, cursor, chunk, file, parent, encoded_classname=None):
+        start_cursor = cursor.copy(link_refs=True)
+        num_bytes, version = _numbytes_version(cursor, chunk)
 
         if version in cls._class_versions:
             versioned_cls = cls._class_versions[version]
-            self = versioned_cls.__new__(versioned_cls)
-            self._cursor = cursor
-            self._file = file
-            self._parent = parent
-            self._members = {}
-            self._bases = []
+            self = versioned_cls.read(
+                start_cursor, chunk, file, parent, encoded_classname=encoded_classname
+            )
 
         elif num_bytes is not None:
-            self = UnknownClassVersion.read(cursor, file, parent)
-            self._encoded_classname = type(cls).__name__
+            self = UnknownClassVersion.read(
+                start_cursor, chunk, file, parent, encoded_classname=encoded_classname
+            )
             self._instance_version = version
             self._known_versions = cls._class_versions
 
@@ -247,13 +266,11 @@ class ModelVersions(object):
             )
 
         self._num_bytes = num_bytes
-        moved_cursor = self.read_members(moved_cursor)
+
+        self.read_bases_members(cursor)
+
         _numbytes_check(
-            cursor, moved_cursor, num_bytes, classname_pretty(self), file.file_path
+            start_cursor, cursor, num_bytes, classname_pretty(self), file.file_path
         )
 
         return self
-
-    @property
-    def num_bytes(self):
-        return self._num_bytes
