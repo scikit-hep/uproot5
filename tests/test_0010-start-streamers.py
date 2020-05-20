02e668781bfd3ebad54143a4b6669395857eca9b
Jim Pivarski <jpivarski@gmail.com>
2020-05-20 10:44:09 -0500

    [skip ci] Stub test.

---
+++ tests/test_0010-start-streamers.py
@@ -0,0 +1,18 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+
+import numpy
+import pytest
+import skhep_testdata
+
+import uproot4
+import uproot4.generate
+import uproot4.reading
+
+
+def test():
+    with uproot4.open(skhep_testdata.data_path("uproot-histograms.root")) as directory:
+        pass
