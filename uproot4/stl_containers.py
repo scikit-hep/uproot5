124c6d137c79a7bfb1d1588451f586ce4736389d
Jim Pivarski <jpivarski@gmail.com>
2020-06-19 14:41:16 -0500

    Developed STLVector and STLMap.

--- uproot4/stl_containers.py
+++ uproot4/stl_containers.py
@@ -2,16 +2,22 @@
 
 from __future__ import absolute_import
 
+import types
+
 try:
     from collections.abc import Sequence
     # from collections.abc import Set
-    # from collections.abc import Mapping
+    from collections.abc import Mapping
+    from collections.abc import KeysView
+    from collections.abc import ValuesView
 except ImportError:
     from collections import Sequence
     # from collections import Set
-    # from collections import Mapping
+    from collections import Mapping
+    KeysView = None
+    ValuesView = None
 
-# import numpy
+import numpy
 
 import uproot4._util
 
@@ -27,7 +33,7 @@ def _tostring(value):
         return str(value)
 
 
-def _str_with_ellipsis(tostring, length, lbracket, rbracket, limit_value):
+def _str_with_ellipsis(tostring, length, lbracket, rbracket, limit):
     leftlen = len(lbracket)
     rightlen = len(rbracket)
     left, right, i, j, done = [], [], 0, length - 1, False
@@ -39,7 +45,7 @@ def _str_with_ellipsis(tostring, length, lbracket, rbracket, limit_value):
         x = tostring(i) + ("" if i == length - 1 else ", ")
         i += 1
         dotslen = 0 if i > j else 5
-        if leftlen + rightlen + len(x) + dotslen > limit_value:
+        if leftlen + rightlen + len(x) + dotslen > limit:
             break
         left.append(x)
         leftlen += len(x)
@@ -50,7 +56,7 @@ def _str_with_ellipsis(tostring, length, lbracket, rbracket, limit_value):
         y = tostring(j) + ("" if j == length - 1 else ", ")
         j -= 1
         dotslen = 0 if i > j else 5
-        if leftlen + rightlen + len(y) + dotslen > limit_value:
+        if leftlen + rightlen + len(y) + dotslen > limit:
             break
         right.insert(0, y)
         rightlen += len(y)
@@ -68,31 +74,173 @@ def _str_with_ellipsis(tostring, length, lbracket, rbracket, limit_value):
 
 
 class STLVector(STLContainer, Sequence):
-    def __init__(self, content):
-        self._content = content
+    def __init__(self, values):
+        if isinstance(values, types.GeneratorType):
+            values = numpy.asarray(list(values))
+        elif isinstance(values, (list, tuple)):
+            values = numpy.asarray(values)
 
-    def __str__(self, limit_value=85):
+        self._values = values
+
+    def __str__(self, limit=85):
         def tostring(i):
-            return _tostring(self[i])
+            return _tostring(self._values[i])
 
-        return _str_with_ellipsis(tostring, len(self), "[", "]", limit_value)
+        return _str_with_ellipsis(tostring, len(self), "[", "]", limit)
 
-    def __repr__(self, limit_value=85):
+    def __repr__(self, limit=85):
         return "<STLVector {0} at 0x{1:012x}>".format(
-            self.__str__(limit_value=limit_value - 30), id(self)
+            self.__str__(limit=limit - 30), id(self)
         )
 
     def __getitem__(self, where):
-        return self._content[where]
+        return self._values[where]
 
     def __len__(self):
-        return len(self._content)
+        return len(self._values)
 
     def __contains__(self, what):
-        return what in self._content
+        return what in self._values
 
     def __iter__(self):
-        return iter(self._content)
+        return iter(self._values)
 
     def __reversed__(self):
-        return STLVector(self._content[::-1])
+        return STLVector(self._values[::-1])
+
+    def __eq__(self, other):
+        if isinstance(other, STLVector):
+            return self._values == other._values
+        elif isinstance(other, Sequence):
+            return self._values == other
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self == other
+
+
+class STLMap(STLContainer, Mapping):
+    def __init__(self, keys, values):
+        if KeysView is not None and isinstance(keys, KeysView):
+            keys = numpy.asarray(list(keys))
+        elif isinstance(keys, types.GeneratorType):
+            keys = numpy.asarray(list(keys))
+        else:
+            keys = numpy.asarray(keys)
+
+        if ValuesView is not None and isinstance(values, ValuesView):
+            values = numpy.asarray(list(values))
+        elif isinstance(values, types.GeneratorType):
+            values = numpy.asarray(list(values))
+
+        if len(keys) != len(values):
+            raise ValueError("number of keys must be equal to the number of values")
+
+        index = numpy.argsort(keys)
+
+        self._keys = keys[index]
+        try:
+            self._values = values[index]
+        except Exception:
+            self._values = numpy.asarray(values)[index]
+
+    def __str__(self, limit=85):
+        def tostring(i):
+            return repr(self._keys[i]) + ": " + _tostring(self._values[i])
+
+        return _str_with_ellipsis(tostring, len(self), "{", "}", limit)
+
+    def __repr__(self, limit=85):
+        return "<STLMap {0} at 0x{1:012x}>".format(
+            self.__str__(limit=limit - 30), id(self)
+        )
+
+    def __getitem__(self, where):
+        where = numpy.asarray(where)
+        index = numpy.searchsorted(self._keys.astype(where.dtype), where, side="left")
+
+        if uproot4._util.isint(index):
+            if index < len(self._keys) and self._keys[index] == where:
+                return self._values[index]
+            else:
+                raise KeyError(where)
+
+        elif len(self._keys) == 0:
+            values = numpy.empty(len(index))
+            return numpy.ma.MaskedArray(values, True)
+
+        else:
+            index[index >= len(self._keys)] = 0
+            mask = self._keys[index] != where
+            return numpy.ma.MaskedArray(self._values[index], mask)
+
+    def get(self, where, default=None):
+        where = numpy.asarray(where)
+        index = numpy.searchsorted(self._keys.astype(where.dtype), where, side="left")
+
+        if uproot4._util.isint(index):
+            if index < len(self._keys) and self._keys[index] == where:
+                return self._values[index]
+            else:
+                return default
+
+        elif len(self._keys) == 0:
+            return numpy.array([default])[numpy.zeros(len(index), numpy.int32)]
+
+        else:
+            index[index >= len(self._keys)] = 0
+            matches = self._keys[index] == where
+            values = self._values[index]
+            defaults = numpy.array([default])[numpy.zeros(len(index), numpy.int32)]
+            return numpy.where(matches, values, defaults)
+
+    def __len__(self):
+        return len(self._keys)
+
+    def __iter__(self):
+        return iter(self._keys)
+
+    def __contains__(self, where):
+        where = numpy.asarray(where)
+        index = numpy.searchsorted(self._keys.astype(where.dtype), where, side="left")
+
+        if uproot4._util.isint(index):
+            if index < len(self._keys) and self._keys[index] == where:
+                return True
+            else:
+                return False
+
+        else:
+            return False
+
+    def keys(self):
+        return self._keys
+
+    def values(self):
+        return self._values
+
+    def items(self):
+        return numpy.transpose(numpy.vstack([self._keys, self._values]))
+
+    def __eq__(self, other):
+        if isinstance(other, Mapping):
+            if not isinstance(other, STLMap):
+                other = STLMap(other.keys(), other.values())
+            keys = other._keys
+            values = other._values
+        else:
+            return False
+
+        if len(self._keys) != len(keys):
+            return False
+
+        keys_same = self._keys == keys
+        values_same = self._values == values
+        if isinstance(keys_same, bool) and isinstance(values_same, bool):
+            return keys_same and values_same
+        else:
+            return numpy.logical_and(keys_same, values_same).all()
+
+    def __ne__(self, other):
+        return not self == other
