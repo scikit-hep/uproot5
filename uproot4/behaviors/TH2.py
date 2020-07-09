ffa2dc99ed13a28987fabd851297a2994d84e1c9
Jim Pivarski <jpivarski@gmail.com>
2020-07-09 13:17:53 -0500

    1D and 2D histograms can both be converted to NumPy format.

--- uproot4/behaviors/TH2.py
+++ uproot4/behaviors/TH2.py
@@ -7,10 +7,11 @@ import numpy
 import uproot4.models.TArray
 
 
-class TH1(object):
+class TH2(object):
     @property
     def np(self):
         xaxis = self.member("fXaxis")
+        yaxis = self.member("fYaxis")
 
         xaxis_fNbins = xaxis.member("fNbins")
         xedges = numpy.empty(xaxis_fNbins + 3, dtype=numpy.float64)
@@ -25,9 +26,22 @@ class TH1(object):
         else:
             xedges[1:-1] = xaxis_fXbins
 
+        yaxis_fNbins = yaxis.member("fNbins")
+        yedges = numpy.empty(yaxis_fNbins + 3, dtype=numpy.float64)
+        yedges[0] = -numpy.inf
+        yedges[-1] = numpy.inf
+
+        yaxis_fXbins = yaxis.member("fXbins", none_if_missing=True)
+        if yaxis_fXbins is None or len(yaxis_fXbins) == 0:
+            yedges[1:-1] = numpy.linspace(
+                yaxis.member("fXmin"), yaxis.member("fXmax"), yaxis_fNbins + 1
+            )
+        else:
+            yedges[1:-1] = yaxis_fXbins
+
         for base in self.bases:
             if isinstance(base, uproot4.models.TArray.Model_TArray):
                 values = numpy.array(base, dtype=base.dtype.newbyteorder("="))
                 break
 
-        return values, xedges
+        return values.reshape(xaxis_fNbins + 2, yaxis_fNbins + 2), xedges, yedges
