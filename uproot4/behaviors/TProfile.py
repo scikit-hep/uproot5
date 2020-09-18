e89d2b6c667d2ee1037a3de9cc8bf25a9b60ef21
Jim Pivarski <jpivarski@gmail.com>
2020-09-18 13:17:49 -0500

    Refactor effective_entries, values, and values_errors for TProfile to use in TProfile2D.

--- uproot4/behaviors/TProfile.py
+++ uproot4/behaviors/TProfile.py
@@ -18,6 +18,111 @@ _kERRORSPREADI = 2
 _kERRORSPREADG = 3
 
 
+# closely follows the ROOT function, using the same names (with 'root_' prepended)
+# https://github.com/root-project/root/blob/ffc7c588ac91aca30e75d356ea971129ee6a836a/hist/hist/src/TProfileHelper.h#L141-L163
+def _effective_entries_1d(fBinEntries, fBinSumw2, fNcells):
+    root_sumOfWeights = fBinEntries
+    root_sumOfWeights = numpy.array(root_sumOfWeights, dtype=numpy.float64)
+
+    root_sumOfWeightSquare = fBinSumw2
+    root_sumOfWeightSquare = numpy.array(
+        root_sumOfWeightSquare, dtype=numpy.float64
+    )
+
+    if len(root_sumOfWeightSquare) == 0 or len(root_sumOfWeightSquare) != fNcells:
+        return root_sumOfWeights
+
+    positive = root_sumOfWeightSquare > 0
+
+    out = numpy.zeros(len(root_sumOfWeights), dtype=numpy.float64)
+    out[positive] = (
+        root_sumOfWeights[positive] ** 2 / root_sumOfWeightSquare[positive]
+    )
+    return out
+
+# duplicates the first part of '_values_errors_1d'
+def _values_1d(fBinEntries):
+    root_sum = fBinEntries
+    root_sum = numpy.array(root_sum, dtype=numpy.float64)
+    nonzero = root_sum != 0
+
+    (root_cont,) = self.base(uproot4.models.TArray.Model_TArray)
+    root_cont = numpy.asarray(root_cont, dtype=numpy.float64)
+
+    root_contsum = numpy.zeros(len(root_cont), dtype=numpy.float64)
+    root_contsum[nonzero] = root_cont[nonzero] / root_sum[nonzero]
+
+    return root_contsum
+
+# closely follows the ROOT function, using the same names (with 'root_' prepended)
+# https://github.com/root-project/root/blob/ffc7c588ac91aca30e75d356ea971129ee6a836a/hist/hist/src/TProfileHelper.h#L660-L721
+def _values_errors_1d(error_mode, fBinEntries, root_cont, fSumw2, fNcells, fBinSumw2):
+    if error_mode is None or error_mode == _kERRORMEAN or error_mode == "":
+        error_mode = _kERRORMEAN
+    elif error_mode == _kERRORSPREAD or error_mode == "s":
+        error_mode = _kERRORSPREAD
+    elif error_mode == _kERRORSPREADI or error_mode == "i":
+        error_mode = _kERRORSPREADI
+    elif error_mode == _kERRORSPREADG or error_mode == "g":
+        error_mode = _kERRORSPREADG
+    else:
+        raise ValueError(
+            "error_mode must be None/0/'' for error-on-mean,\n"
+            "                        1/'s' for spread (variance),\n"
+            "                        2/'i' for integer spread (using sqrt(12)),\n"
+            "                     or 3/'g' for Gaussian spread\n"
+            "                    not "
+            + repr(error_mode)
+            + "see https://root.cern.ch/doc/master/classTProfile.html"
+        )
+
+    root_sum = fBinEntries
+    root_sum = numpy.array(root_sum, dtype=numpy.float64)
+    nonzero = root_sum != 0
+
+    root_contsum = numpy.zeros(len(root_cont), dtype=numpy.float64)
+    root_contsum[nonzero] = root_cont[nonzero] / root_sum[nonzero]
+
+    if error_mode == _kERRORSPREADG:
+        out = numpy.zeros(len(root_cont), dtype=numpy.float64)
+        out[nonzero] = 1.0 / numpy.sqrt(root_sum[nonzero])
+        return root_contsum, out
+
+    root_err2 = fSumw2
+    if root_err2 is None or len(root_err2) != fNcells:
+        root_err2 = numpy.zeros(len(root_cont), dtype=numpy.float64)
+    else:
+        root_err2 = numpy.array(root_err2, dtype=numpy.float64)
+
+    root_neff = _effective_entries_1d(fBinEntries, fBinSumw2, fNcells)
+
+    root_eprim2 = numpy.zeros(len(root_cont), dtype=numpy.float64)
+    root_eprim2[nonzero] = abs(
+        root_err2[nonzero] / root_sum[nonzero] - root_contsum[nonzero] ** 2
+    )
+    root_eprim = numpy.sqrt(root_eprim2)
+
+    if error_mode == _kERRORSPREADI:
+        numer = numpy.ones(len(root_cont), dtype=numpy.float64)
+        denom = numpy.full(len(root_cont), numpy.sqrt(12 * root_neff))
+
+        eprim_nonzero = root_eprim != 0
+        numer[eprim_nonzero] = root_eprim[eprim_nonzero]
+        denom[eprim_nonzero] = numpy.sqrt(root_neff[eprim_nonzero])
+
+        out = numpy.zeros(len(root_cont), dtype=numpy.float64)
+        out[nonzero] = numer[nonzero] / denom[nonzero]
+        return root_contsum, out
+
+    if error_mode == _kERRORSPREAD:
+        root_eprim[~nonzero] = 0.0
+        return root_contsum, root_eprim
+
+    out = numpy.zeros(len(root_cont), dtype=numpy.float64)
+    out[nonzero] = root_eprim[nonzero] / numpy.sqrt(root_neff[nonzero])
+    return root_contsum, out
+
+
 class Profile(uproot4.behaviors.TH1.Histogram):
     """
     Abstract class for profile plots.
@@ -65,116 +170,29 @@ class TProfile(Profile):
             raise ValueError("axis must be 0 or 'x' for a TH1")
 
     def effective_entries(self):
-        # closely follows the ROOT function, using the same names (with 'root_' prepended)
-        # https://github.com/root-project/root/blob/ffc7c588ac91aca30e75d356ea971129ee6a836a/hist/hist/src/TProfileHelper.h#L141-L163
-
-        root_sumOfWeights = self.member("fBinEntries")
-        root_sumOfWeights = numpy.array(root_sumOfWeights, dtype=numpy.float64)
-
-        root_sumOfWeightSquare = self.member("fBinSumw2")
-        root_sumOfWeightSquare = numpy.array(
-            root_sumOfWeightSquare, dtype=numpy.float64
+        return _effective_entries_1d(
+            self.member("fBinEntries"),
+            self.member("fBinSumw2"),
+            self.member("fNcells"),
         )
-
-        if len(root_sumOfWeightSquare) == 0 or len(
-            root_sumOfWeightSquare
-        ) != self.member("fNcells"):
-            return root_sumOfWeights
-
-        positive = root_sumOfWeightSquare > 0
-
-        out = numpy.zeros(len(root_sumOfWeights), dtype=numpy.float64)
-        out[positive] = (
-            root_sumOfWeights[positive] ** 2 / root_sumOfWeightSquare[positive]
-        )
-        return out
 
     def values(self):
-        # duplicates the first part of 'values_errors'
-
-        root_sum = self.member("fBinEntries")
-        root_sum = numpy.array(root_sum, dtype=numpy.float64)
-        nonzero = root_sum != 0
-
-        (root_cont,) = self.base(uproot4.models.TArray.Model_TArray)
-        root_cont = numpy.array(root_cont, dtype=numpy.float64)
-
-        root_contsum = numpy.zeros(len(root_cont), dtype=numpy.float64)
-        root_contsum[nonzero] = root_cont[nonzero] / root_sum[nonzero]
-
-        return root_contsum
+        return _values_1d(self.member("fBinEntries"))
 
     def values_errors(self, error_mode=""):
-        # closely follows the ROOT function, using the same names (with 'root_' prepended)
-        # https://github.com/root-project/root/blob/ffc7c588ac91aca30e75d356ea971129ee6a836a/hist/hist/src/TProfileHelper.h#L660-L721
-
-        if error_mode is None or error_mode == _kERRORMEAN or error_mode == "":
-            error_mode = _kERRORMEAN
-        elif error_mode == _kERRORSPREAD or error_mode == "s":
-            error_mode = _kERRORSPREAD
-        elif error_mode == _kERRORSPREADI or error_mode == "i":
-            error_mode = _kERRORSPREADI
-        elif error_mode == _kERRORSPREADG or error_mode == "g":
-            error_mode = _kERRORSPREADG
-        else:
-            raise ValueError(
-                "error_mode must be None/0/'' for error-on-mean,\n"
-                "                        1/'s' for spread (variance),\n"
-                "                        2/'i' for integer spread (using sqrt(12)),\n"
-                "                     or 3/'g' for Gaussian spread\n"
-                "                    not "
-                + repr(error_mode)
-                + "see https://root.cern.ch/doc/master/classTProfile.html"
-            )
-
-        root_sum = self.member("fBinEntries")
-        root_sum = numpy.array(root_sum, dtype=numpy.float64)
-        nonzero = root_sum != 0
-
         (root_cont,) = self.base(uproot4.models.TArray.Model_TArray)
-        root_cont = numpy.array(root_cont, dtype=numpy.float64)
-
-        root_contsum = numpy.zeros(len(root_cont), dtype=numpy.float64)
-        root_contsum[nonzero] = root_cont[nonzero] / root_sum[nonzero]
-
-        if error_mode == _kERRORSPREADG:
-            out = numpy.zeros(len(root_cont), dtype=numpy.float64)
-            out[nonzero] = 1.0 / numpy.sqrt(root_sum[nonzero])
-            return root_contsum, out
-
-        root_err2 = self.member("fSumw2", none_if_missing=True)
-        if root_err2 is None or len(root_err2) != self.member("fNcells"):
-            root_err2 = numpy.zeros(len(root_cont), dtype=numpy.float64)
-        else:
-            root_err2 = numpy.array(root_err2, dtype=numpy.float64)
-
-        root_neff = self.effective_entries()
-
-        root_eprim2 = numpy.zeros(len(root_cont), dtype=numpy.float64)
-        root_eprim2[nonzero] = abs(
-            root_err2[nonzero] / root_sum[nonzero] - root_contsum[nonzero] ** 2
+        root_cont = numpy.asarray(root_cont, dtype=numpy.float64)
+        fSumw2 = self.member("fSumw2", none_if_missing=True)
+        if fSumw2 is not None:
+            fSumw2 = numpy.asarray(fSumw2)
+        return _values_errors_1d(
+            error_mode,
+            numpy.asarray(self.member("fBinEntries")),
+            root_cont,
+            fSumw2,
+            self.member("fNcells"),
+            numpy.asarray(self.member("fBinSumw2")),
         )
-        root_eprim = numpy.sqrt(root_eprim2)
-
-        if error_mode == _kERRORSPREADI:
-            numer = numpy.ones(len(root_cont), dtype=numpy.float64)
-            denom = numpy.full(len(root_cont), numpy.sqrt(12 * root_neff))
-
-            eprim_nonzero = root_eprim != 0
-            numer[eprim_nonzero] = root_eprim[eprim_nonzero]
-            denom[eprim_nonzero] = numpy.sqrt(root_neff[eprim_nonzero])
-
-            out = numpy.zeros(len(root_cont), dtype=numpy.float64)
-            out[nonzero] = numer[nonzero] / denom[nonzero]
-            return root_contsum, out
-
-        if error_mode == _kERRORSPREAD:
-            root_eprim[~nonzero] = 0.0
-            return root_contsum, root_eprim
-
-        out = numpy.zeros(len(root_cont), dtype=numpy.float64)
-        out[nonzero] = root_eprim[nonzero] / numpy.sqrt(root_neff[nonzero])
-        return root_contsum, out
 
     def to_numpy(self, flow=False, dd=False, errors=False, error_mode=0):
         if errors:
--- uproot4/behaviors/TProfile2D.py
+++ uproot4/behaviors/TProfile2D.py
@@ -26,11 +26,35 @@ class TProfile2D(uproot4.behaviors.TProfile.Profile):
         else:
             raise ValueError("axis must be 0, 1 or 'x', 'y' for a TProfile2D")
 
+    def effective_entries(self):
+        fBinEntries = self.member("fBinEntries")
+        out = _effective_entries_1d(
+            fBinEntries.reshape(-1),
+            self.member("fBinSumw2").reshape(-1),
+            self.member("fNcells"),
+        )
+        return out.reshape(shape)
+
     def values(self):
-        raise NotImplementedError(repr(self))
+        fBinEntries = self.member("fBinEntries")
+        out = uproot4.behaviors.TProfile._values_1d(fBinEntries.reshape(-1))
+        return out.reshape(fBinEntries.shape)
 
     def values_errors(self, error_mode=""):
-        raise NotImplementedError(repr(self))
+        (root_cont,) = self.base(uproot4.models.TArray.Model_TArray)
+        root_cont = numpy.asarray(root_cont, dtype=numpy.float64)
+        fSumw2 = self.member("fSumw2", none_if_missing=True)
+        if fSumw2 is not None:
+            fSumw2 = numpy.asarray(fSumw2).reshape(-1)
+        out = uproot4.behaviors.TProfile._values_errors_1d(
+            error_mode,
+            numpy.asarray(self.member("fBinEntries")).reshape(-1),
+            root_cont.reshape(-1),
+            fSumw2,
+            self.member("fNcells"),
+            numpy.asarray(self.member("fBinSumw2")).reshape(-1),
+        )
+        return out.reshape(root_cont.shape)
 
     def to_numpy(self, flow=False, dd=False, errors=False, error_mode=0):
         raise NotImplementedError(repr(self))
