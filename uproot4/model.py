e0c3e72b6046b92f405da4816318a62b7b4e47a7
Jim Pivarski <jpivarski@gmail.com>
2020-11-20 13:57:56 -0600

    Add fallback for wrong class version.

--- uproot4/model.py
+++ uproot4/model.py
@@ -758,6 +758,25 @@ class Model(object):
 
         self.read_numbytes_version(chunk, cursor, context)
 
+        if (
+            issubclass(cls, VersionedModel) and
+            self._instance_version != classname_version(cls.__name__) and
+            self._instance_version is not None
+        ):
+            correct_cls = file.class_named(self.classname, self._instance_version)
+            if classname_version(correct_cls.__name__) != classname_version(cls.__name__):
+                cursor.move_to(self._cursor.index)
+                context["breadcrumbs"] = old_breadcrumbs
+                return correct_cls.read(
+                    chunk,
+                    cursor,
+                    context,
+                    file,
+                    selffile,
+                    parent,
+                    concrete=concrete,
+                )
+
         if context.get("in_TBranch", False):
             if self._num_bytes is None and self._instance_version != self.class_version:
                 self._instance_version = None
