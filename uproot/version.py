46516f461d94056e4fb3e2a03c73636c8a2a6439
Jim Pivarski <jpivarski@users.noreply.github.com>
2020-12-01 15:51:59 -0600

    The actual version is 4.0.0rc1.

--- uproot/version.py
+++ uproot/version.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 
 import re
 
-__version__ = "1.0.0rc1"
+__version__ = "4.0.0rc1"
 version = __version__
 version_info = tuple(re.split(r"[-\.]", __version__))
 
