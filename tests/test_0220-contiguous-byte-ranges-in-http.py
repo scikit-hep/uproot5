8178bdaffb9f3882d02153f2078dc3e4c4fba333
Jim Pivarski <jpivarski@users.noreply.github.com>
2020-12-07 16:22:19 -0600

    Some HTTP servers combine contiguous byte ranges. (#221)

---
+++ tests/test_0220-contiguous-byte-ranges-in-http.py
@@ -0,0 +1,21 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import numpy
+import pytest
+
+import uproot
+
+
+def test():
+    with uproot.open(
+        "https://starterkit.web.cern.ch/starterkit/data/advanced-python-2019/RD_distribution.root:tree"
+    ) as f:
+        whole_branch = f["vchi2_b"].array(library="np")
+        assert whole_branch[0] == 5.234916687011719
+        assert whole_branch[-1] == 12.466843605041504
+
+        whole_branch = f["mu_pt_sum"].array(library="np")
+        assert whole_branch[0] == 26.4675350189209
+        assert whole_branch[-1] == 39.84319305419922
