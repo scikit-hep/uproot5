99ffff4652f3b47196f41b2d0726a70db7cb56fa
Giordon Stark <kratsg@gmail.com>
2021-01-08 16:23:06 -0500

    fix flake8

--- uproot/behaviors/TGraphAsymmErrors.py
+++ uproot/behaviors/TGraphAsymmErrors.py
@@ -8,9 +8,8 @@ from __future__ import absolute_import
 
 import numpy
 
-import uproot
-
 # dict_keys(['@fUniqueID', '@fBits', 'fName', 'fTitle', 'fLineColor', 'fLineStyle', 'fLineWidth', 'fFillColor', 'fFillStyle', 'fMarkerColor', 'fMarkerStyle', 'fMarkerSize', 'fNpoints', 'fX', 'fY', 'fFunctions', 'fHistogram', 'fMinimum', 'fMaximum', 'fEXlow', 'fEXhigh', 'fEYlow', 'fEYhigh'])
+
 
 class TGraphAsymmErrors(object):
     """
@@ -41,7 +40,9 @@ class TGraphAsymmErrors(object):
             _exhigh, _eyhigh = self.member("fEXhigh"), self.member("fEYhigh")
 
             _low = numpy.asarray([_exlow, _eylow], dtype=_exlow.dtype.newbyteorder("="))
-            _high = numpy.asarray([_exhigh, _eyhigh], dtype=_exhigh.dtype.newbyteorder("="))
+            _high = numpy.asarray(
+                [_exhigh, _eyhigh], dtype=_exhigh.dtype.newbyteorder("=")
+            )
             errors = (_low, _high)
             self._errors = errors
 
