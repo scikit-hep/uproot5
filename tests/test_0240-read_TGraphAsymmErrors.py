f67c67c7d795ef02e87acbd996695eb4afc39a57
Giordon Stark <kratsg@gmail.com>
2021-01-11 18:12:57 -0500

    feat: Add TGraphAsymmErrors (#240)
    * add initial template for tgraphasymmerrors behaviors
    
    * fix flake8
    
    * update with newer API
    
    * hrm
    
    * add test for the new features added
    
    * check length of output
    
    * add requests to dev requirements
    
    * whoops should be in test
    
    * update workflow for now, needs to be rfactored later
    
    * remove from dev
    
    * switch out the fixture
    
    * bytes
    
    * Just a little formatting.
    
    * And more documentation.
    
    Co-authored-by: Jim Pivarski <jpivarski@gmail.com>

---
+++ tests/test_0240-read_TGraphAsymmErrors.py
@@ -0,0 +1,75 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import numpy
+import pytest
+
+import uproot
+
+requests = pytest.importorskip("requests")
+
+
+@pytest.mark.network
+@pytest.fixture(scope="module")
+def datafile(tmpdir_factory):
+    response = requests.get(
+        "https://www.hepdata.net/download/table/ins1755298/Expected%20limit%201lbb/3/root"
+    )
+    response.raise_for_status()
+    tmp_file = tmpdir_factory.mktemp("data").join(
+        "HEPData-ins1755298-v3-Expected_limit_1lbb.root"
+    )
+    with open(str(tmp_file), "wb") as f:
+        f.write(response.content)
+    yield str(tmp_file)
+
+
+@pytest.fixture
+def graph(datafile):
+    with uproot.open(datafile) as f:
+        yield f["Expected limit 1lbb/Graph1D_y1"]
+
+
+def test_interpretation(graph):
+    assert graph.classname == "TGraphAsymmErrors"
+    assert graph.behaviors[0] == uproot.behaviors.TGraphAsymmErrors.TGraphAsymmErrors
+
+    assert "fX" in graph.all_members.keys()
+    assert "fY" in graph.all_members.keys()
+    assert "fEXlow" in graph.all_members.keys()
+    assert "fEYlow" in graph.all_members.keys()
+    assert "fEXhigh" in graph.all_members.keys()
+    assert "fEYhigh" in graph.all_members.keys()
+
+
+@pytest.mark.parametrize("axis", [-2, -1, 0, 1, "x", "y"])
+def test_values_single(graph, axis):
+    values = graph.values(axis=axis)
+    assert isinstance(values, numpy.ndarray)
+    assert values.shape == (162,)
+
+
+@pytest.mark.parametrize("axis", [-2, -1, 0, 1, "x", "y"])
+@pytest.mark.parametrize("which", ["low", "high", "mean", "diff"])
+def test_errors_single(graph, axis, which):
+    errors = graph.errors(axis=axis, which=which)
+    assert isinstance(errors, numpy.ndarray)
+    assert errors.shape == (162,)
+
+
+@pytest.mark.parametrize("axis", ["both"])
+def test_values_double(graph, axis):
+    values = graph.values(axis=axis)
+    assert len(values) == 2
+    assert all(isinstance(arr, numpy.ndarray) for arr in values)
+    assert all(arr.shape == (162,) for arr in values)
+
+
+@pytest.mark.parametrize("axis", ["both"])
+@pytest.mark.parametrize("which", ["low", "high", "mean", "diff"])
+def test_errors_double(graph, axis, which):
+    errors = graph.errors(axis=axis, which=which)
+    assert len(errors) == 2
+    assert all(isinstance(arr, numpy.ndarray) for arr in errors)
+    assert all(arr.shape == (162,) for arr in errors)
---
+++ uproot/behaviors/TGraphAsymmErrors.py
@@ -0,0 +1,89 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+"""
+This module defines the behaviors of ``TGraphAsymmErrors``.
+"""
+
+from __future__ import absolute_import
+
+import numpy
+
+# ['@fUniqueID', '@fBits', 'fName', 'fTitle', 'fLineColor', 'fLineStyle',
+#  'fLineWidth', 'fFillColor', 'fFillStyle', 'fMarkerColor', 'fMarkerStyle',
+#  'fMarkerSize', 'fNpoints', 'fX', 'fY', 'fFunctions', 'fHistogram',
+#  'fMinimum', 'fMaximum', 'fEXlow', 'fEXhigh', 'fEYlow', 'fEYhigh']
+
+
+class TGraphAsymmErrors(object):
+    """
+    Behaviors for TGraphAsymmErrors: get values and errors as NumPy arrays.
+    """
+
+    def _normalize_array(self, key):
+        values = self.member(key)
+        return numpy.asarray(values, dtype=values.dtype.newbyteorder("="))
+
+    def _reduce_errors(self, which, lowkey, highkey):
+        if which == "low":
+            return self._normalize_array(lowkey)
+        elif which == "high":
+            return self._normalize_array(highkey)
+        elif which == "mean":
+            return (
+                self._normalize_array(highkey) + self._normalize_array(lowkey)
+            ) / 2.0
+        elif which == "diff":
+            return self._normalize_array(highkey) - self._normalize_array(lowkey)
+
+    def values(self, axis="both"):
+        """
+        Args:
+            axis (int or str): If ``0``, ``-2``, or ``"x"``, get the *x* axis.
+                If ``1``, ``-1``, or ``"y"``, get the *y* axis. If ``"both"``,
+                get ``"x"`` and ``"y"`` axes as a 2-tuple.
+
+        Returns the values of all points in the scatter plot, either as a
+        1-dimensional NumPy array (if ``axis`` selects only one) or as a 2-tuple
+        (if ``axis="both"``).
+        """
+        if axis in [0, -2, "x"]:
+            return self._normalize_array("fX")
+        elif axis in [1, -1, "y"]:
+            return self._normalize_array("fY")
+        elif axis == "both":
+            return (self.values("x"), self.values("y"))
+        else:
+            raise ValueError(
+                "axis must be 0 (-2, 'x'), 1 (-1, 'y'), or 'both' for a TGraphAsymmErrors"
+            )
+
+    def errors(self, which, axis="both"):
+        """
+        Args:
+            which (str): If ``"low"`` or ``"high"``, get the low-side or
+                high-side error. If ``"mean"``, get the average. If ``"diff"``,
+                get their difference.
+            axis (int or str): If ``0``, ``-2``, or ``"x"``, get the *x* axis.
+                If ``1``, ``-1``, or ``"y"``, get the *y* axis. If ``"both"``,
+                get ``"x"`` and ``"y"`` axes as a 2-tuple.
+
+        Returns the errors in all points in the scatter plot, either as a
+        1-dimensional NumPy array (if ``axis`` selects only one) or as a 2-tuple
+        (if ``axis="both"``). The value of ``which`` does not affect the return
+        type.
+        """
+        if which not in ["low", "high", "mean", "diff"]:
+            raise ValueError(
+                "which must be 'low', 'high', 'mean', or 'diff' for a TGraphAsymmErrors"
+            )
+
+        if axis in [0, -2, "x"]:
+            return self._reduce_errors(which, "fEXlow", "fEXhigh")
+        elif axis in [1, -1, "y"]:
+            return self._reduce_errors(which, "fEYlow", "fEYhigh")
+        elif axis == "both":
+            return (self.errors(which, "x"), self.errors(which, "y"))
+        else:
+            raise ValueError(
+                "axis must be 0 (-2, 'x'), 1 (-1, 'y'), or 'both' for a TGraphAsymmErrors"
+            )
