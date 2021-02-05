284629957af3a7f6deaf768b03aa0932d7630329
Jim Pivarski <jpivarski@gmail.com>
2021-02-04 18:17:35 -0600

    The next version number will be 4.0.2rc2.

--- uproot/version.py
+++ uproot/version.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 
 import re
 
-__version__ = "4.0.2rc1"
+__version__ = "4.0.2rc2"
 version = __version__
 version_info = tuple(re.split(r"[-\.]", __version__))
 
