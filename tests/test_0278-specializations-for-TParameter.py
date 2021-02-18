446da9f5e9417a8aa2dc36958b80b9ee195f713b
Jim Pivarski <jpivarski@users.noreply.github.com>
2021-02-18 10:46:44 -0600

    Add specialized behaviors for TParameter, including generic dispatch for '<number>' templates. (#278)

---
+++ tests/test_0278-specializations-for-TParameter.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+import numpy as np
+from numpy.testing import assert_array_equal
+import skhep_testdata
+import uproot
+
+
+def test():
+    with uproot.open(skhep_testdata.data_path("uproot-issue64.root")) as file:
+        p = file["events/nbevents"]
+        assert p.value == 500
+        assert p
+        assert int(p) == 500
+        assert float(p) == 500.0
--- uproot/__init__.py
+++ uproot/__init__.py
@@ -191,6 +191,13 @@ def behavior_of(classname):
     assert name.startswith("Model_")
     name = name[6:]
 
+    specialization = None
+    for param in behavior_of._specializations:
+        if name.endswith(param):
+            specialization = param
+            name = name[: -len(param)]
+            break
+
     if name not in globals():
         if name in behavior_of._module_names:
             exec(
@@ -204,12 +211,49 @@ def behavior_of(classname):
             if behavior_cls is not None:
                 globals()[name] = behavior_cls
 
-    return globals().get(name)
+    if specialization is None:
+        return globals().get(name)
+    else:
+        return globals().get(name)(specialization)
 
 
 behavior_of._module_names = [
     module_name
     for loader, module_name, is_pkg in pkgutil.walk_packages(uproot.behaviors.__path__)
+]
+
+behavior_of._specializations = [
+    "_3c_bool_3e_",
+    "_3c_char_3e_",
+    "_3c_unsigned_20_char_3e_",
+    "_3c_short_3e_",
+    "_3c_unsigned_20_short_3e_",
+    "_3c_int_3e_",
+    "_3c_unsigned_20_int_3e_",
+    "_3c_long_3e_",
+    "_3c_unsigned_20_long_3e_",
+    "_3c_long_20_long_3e_",
+    "_3c_unsigned_20_long_20_long_3e_",
+    "_3c_size_5f_t_3e_",
+    "_3c_ssize_5f_t_3e_",
+    "_3c_float_3e_",
+    "_3c_double_3e_",
+    "_3c_long_20_double_3e_",
+    "_3c_Bool_5f_t_3e_",
+    "_3c_Char_5f_t_3e_",
+    "_3c_UChar_5f_t_3e_",
+    "_3c_Short_5f_t_3e_",
+    "_3c_UShort_5f_t_3e_",
+    "_3c_Int_5f_t_3e_",
+    "_3c_UInt_5f_t_3e_",
+    "_3c_Long_5f_t_3e_",
+    "_3c_ULong_5f_t_3e_",
+    "_3c_Long64_5f_t_3e_",
+    "_3c_ULong64_5f_t_3e_",
+    "_3c_Size_5f_t_3e_",
+    "_3c_Float_5f_t_3e_",
+    "_3c_Double_5f_t_3e_",
+    "_3c_LongDouble_5f_t_3e_",
 ]
 
 del pkgutil
