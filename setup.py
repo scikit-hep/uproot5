57b0fe3cf49dd9656543d2afb47f32c1f6619596
Jim Pivarski <jpivarski@users.noreply.github.com>
2021-03-31 14:34:32 -0500

    Cleaning up Uproot (#316)
    * Cleaning up Uproot stage 1: moving requirements files into setup.py.
    
    * Moved 'uproot' to 'src/uproot'.
    
    * Remove the flake8 step since it's failing now and it will be reintroduced with pre-commit.
    
    * python_requires with glob patterns on exclusions
    
    Co-authored-by: Henry Schreiner <henry.fredrick.schreiner@cern.ch>
    
    * flake8 path was wrong, but I should be removing it as an explicit step in CI if I'm adding pre-commit, anyway
    
    Co-authored-by: Henry Schreiner <henry.fredrick.schreiner@cern.ch>
    
    Co-authored-by: Henry Schreiner <henry.fredrick.schreiner@cern.ch>

--- setup.py
+++ setup.py
@@ -6,14 +6,27 @@ from setuptools import setup
 
 def get_version():
     g = {}
-    with open(os.path.join("uproot", "version.py")) as f:
+    with open(os.path.join("src", "uproot", "version.py")) as f:
         exec(f.read(), g)
     return g["__version__"]
 
 
 extras = {
-    "test": open("requirements-test.txt").read().strip().split("\n"),
-    "dev":  open("requirements-dev.txt").read().strip().split("\n"),
+    "dev": [
+        "awkward>=1.0.0",
+        "pandas",
+        "boost_histogram>=0.13",
+        "hist>=1.2",
+    ],
+    "test": [
+        "pytest>=4.6",
+        "flake8",
+        "flake8-print",
+        "scikit-hep-testdata",
+        "lz4",
+        "xxhash",
+        "requests",
+    ],
 }
 extras["all"] = sum(extras.values(), [])
 
