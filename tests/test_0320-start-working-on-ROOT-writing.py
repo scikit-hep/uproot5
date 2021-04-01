afcc32bf46cfc05035de50a46a703ad3d221a433
Jim Pivarski <jpivarski@gmail.com>
2021-04-01 17:19:46 -0500

    Successfully moved a directory and read it in ROOT.

--- tests/test_0320-start-working-on-ROOT-writing.py
+++ tests/test_0320-start-working-on-ROOT-writing.py
@@ -23,12 +23,16 @@ _key_format_big = struct.Struct(">ihiIhhqq")
 _string_size_format_1 = struct.Struct(">B")
 _string_size_format_4 = struct.Struct(">I")
 
-_free_format_1 = struct.Struct(">HII")
-_free_format_2 = struct.Struct(">HQQ")
+_free_format_small = struct.Struct(">HII")
+_free_format_big = struct.Struct(">HQQ")
+
+# /home/jpivarski/storage/data/uproot4-big/issue-288.root
+# /home/jpivarski/storage/data/uproot4-big/issue-90.root
 
 
 @pytest.mark.parametrize(
-    "source", ["uproot-Zmumu.root", "uproot-issue243-new.root", "uproot-issue64.root"]
+    "source",
+    ["uproot-Zmumu.root"],  # , "uproot-issue243-new.root", "uproot-issue64.root"]
 )
 def test(tmp_path, source):
     print(source)
@@ -85,19 +89,174 @@ def test(tmp_path, source):
 
         rawfile.seek(freesegments["fSeekKey"] + freesegments["fKeylen"])
 
+        frees = []
         for _ in range(nfree):
-            version, fFirst, fLast = _free_format_1.unpack(
-                rawfile.read(_free_format_1.size)
+            version, fFirst, fLast = _free_format_small.unpack(
+                rawfile.read(_free_format_small.size)
             )
             if version > 1000:
-                rawfile.seek(rawfile.tell() - _free_format_1.size)
-                version, fFirst, fLast = _free_format_2.unpack(
-                    rawfile.read(_free_format_2.size)
+                rawfile.seek(rawfile.tell() - _free_format_small.size)
+                version, fFirst, fLast = _free_format_big.unpack(
+                    rawfile.read(_free_format_big.size)
                 )
-            # print(f"{version = } {fFirst = } {fLast = } {fLast - fFirst = }")
+            frees.append((version, fFirst, fLast))
 
         directory_start = fBEGIN + fNbytesName
 
+        rawfile.seek(directory_start)
+        directory = read_directory(rawfile)
+
+        rawfile.seek(directory["fSeekKeys"])
+
+        beginning_of_scribble_1 = rawfile.tell()
+
+        header_key = read_key(rawfile)
+        (num_keys,) = _directory_format_num_keys.unpack(
+            rawfile.read(_directory_format_num_keys.size)
+        )
+        keys = [read_key(rawfile) for _ in range(num_keys)]
+
+        end_of_scribble_1 = rawfile.tell()
+
+        beginning_of_scribble_2 = fSeekFree
+        end_of_scribble_2 = fSeekFree + fNbytesFree
+
+        # scribble over the directory and freesegments data
+
+        rawfile.seek(beginning_of_scribble_1)
+        rawfile.write(b"X" * (end_of_scribble_1 - beginning_of_scribble_1))
+
+        rawfile.seek(beginning_of_scribble_2)
+        rawfile.write(b"x" * (end_of_scribble_2 - beginning_of_scribble_2))
+
+        # now make a new directory with the same contents
+
+        _, beginning_of_new_directory, _ = frees[-1]
+
+        rawfile.seek(beginning_of_new_directory)
+        header_key["fSeekKey"] = beginning_of_new_directory
+
+        write_key(rawfile, header_key)
+        rawfile.write(_directory_format_num_keys.pack(num_keys))
+        for key in keys:
+            write_key(rawfile, key)
+
+        end_of_new_directory = rawfile.tell()
+
+        nfree = len(frees[:-1]) + 3
+        fNbytesFree = (
+            _key_format_small.size  # assuming small
+            + length_string(freesegments["fClassName"])
+            + length_string(freesegments["fName"])
+            + length_string(freesegments["fTitle"])
+            + nfree * _free_format_small.size  # assuming small
+        )
+        frees = frees[:-1] + [
+            (1, beginning_of_scribble_1, end_of_scribble_1),
+            (1, beginning_of_scribble_2, end_of_scribble_2),
+            (1, end_of_new_directory + fNbytesFree, 2000000000),
+        ]
+
+        fSeekFree = rawfile.tell()
+
+        freesegments["fSeekKey"] = fSeekFree
+        freesegments["fNbytes"] = fNbytesFree
+        freesegments["fObjlen"] = nfree * _free_format_small.size  # assuming small
+
+        write_key(rawfile, freesegments)
+        for free in frees:
+            version, fFirst, fLast = free
+            if version <= 1000:
+                rawfile.write(_free_format_small.pack(version, fFirst, fLast))
+            else:
+                rawfile.write(_free_format_big.pack(version, fFirst, fLast))
+
+        fEND = rawfile.tell()
+
+        directory["fSeekKeys"] = beginning_of_new_directory
+
+        rawfile.seek(directory_start)
+        write_directory(rawfile, directory)
+
+        rawfile.seek(0)
+        if fVersion <= 1000000:
+            rawfile.write(
+                _file_header_fields_small.pack(
+                    magic,
+                    fVersion,
+                    fBEGIN,
+                    fEND,
+                    fSeekFree,
+                    fNbytesFree,
+                    nfree,
+                    fNbytesName,
+                    fUnits,
+                    fCompress,
+                    fSeekInfo,
+                    fNbytesInfo,
+                    fUUID_version,
+                    fUUID,
+                )
+            )
+        else:
+            rawfile.write(
+                _file_header_fields_big.pack(
+                    magic,
+                    fVersion,
+                    fBEGIN,
+                    fEND,
+                    fSeekFree,
+                    fNbytesFree,
+                    nfree,
+                    fNbytesName,
+                    fUnits,
+                    fCompress,
+                    fSeekInfo,
+                    fNbytesInfo,
+                    fUUID_version,
+                    fUUID,
+                )
+            )
+
+
+def read_string(rawfile):
+    (string_size,) = _string_size_format_1.unpack(rawfile.read(1))
+    if string_size == 255:
+        (string_size,) = _string_size_format_4.unpack(rawfile.read(4))
+    return rawfile.read(string_size).decode(errors="surrogateescape")
+
+
+def length_string(string):
+    bytestring = string.encode(errors="surrogateescape")
+    length = len(bytestring)
+    if len(bytestring) < 255:
+        return len(bytestring) + 1
+    else:
+        return len(bytestring) + 5
+
+
+def write_string(rawfile, string):
+    bytestring = string.encode(errors="surrogateescape")
+    length = len(bytestring)
+    if len(bytestring) < 255:
+        rawfile.write(struct.pack(">B%ds" % length, length, bytestring))
+    else:
+        rawfile.write(struct.pack(">BI%ds" % length, 255, length, bytestring))
+
+
+def read_directory(rawfile):
+    (
+        fVersion,
+        fDatimeC,
+        fDatimeM,
+        fNbytesKeys,
+        fNbytesName,
+        fSeekDir,
+        fSeekParent,
+        fSeekKeys,
+    ) = _directory_format_small.unpack(rawfile.read(_directory_format_small.size))
+
+    if fVersion > 1000:
         rawfile.seek(directory_start)
         (
             fVersion,
@@ -108,41 +267,47 @@ def test(tmp_path, source):
             fSeekDir,
             fSeekParent,
             fSeekKeys,
-        ) = _directory_format_small.unpack(rawfile.read(_directory_format_small.size))
+        ) = _directory_format_big.unpack(rawfile.read(_directory_format_big.size))
 
-        if fVersion > 1000:
-            rawfile.seek(directory_start)
-            (
-                fVersion,
-                fDatimeC,
-                fDatimeM,
-                fNbytesKeys,
-                fNbytesName,
-                fSeekDir,
-                fSeekParent,
-                fSeekKeys,
-            ) = _directory_format_big.unpack(rawfile.read(_directory_format_big.size))
+    return {
+        "fVersion": fVersion,
+        "fDatimeC": fDatimeC,
+        "fDatimeM": fDatimeM,
+        "fNbytesKeys": fNbytesKeys,
+        "fNbytesName": fNbytesName,
+        "fSeekDir": fSeekDir,
+        "fSeekParent": fSeekParent,
+        "fSeekKeys": fSeekKeys,
+    }
 
-        rawfile.seek(fSeekKeys)
 
-        header_key = read_key(rawfile)
-        print(header_key)
-
-        (num_keys,) = _directory_format_num_keys.unpack(
-            rawfile.read(_directory_format_num_keys.size)
+def write_directory(rawfile, directory):
+    if directory["fVersion"] <= 1000:
+        rawfile.write(
+            _directory_format_small.pack(
+                directory["fVersion"],
+                directory["fDatimeC"],
+                directory["fDatimeM"],
+                directory["fNbytesKeys"],
+                directory["fNbytesName"],
+                directory["fSeekDir"],
+                directory["fSeekParent"],
+                directory["fSeekKeys"],
+            )
         )
-        print(num_keys)
-
-        for _ in range(num_keys):
-            key = read_key(rawfile)
-            print(key)
-
-
-def string(rawfile):
-    (string_size,) = _string_size_format_1.unpack(rawfile.read(1))
-    if string_size == 255:
-        (string_size,) = _string_size_format_4.unpack(rawfile.read(4))
-    return rawfile.read(string_size).decode(errors="surrogateescape")
+    else:
+        rawfile.write(
+            _directory_format_big.pack(
+                directory["fVersion"],
+                directory["fDatimeC"],
+                directory["fDatimeM"],
+                directory["fNbytesKeys"],
+                directory["fNbytesName"],
+                directory["fSeekDir"],
+                directory["fSeekParent"],
+                directory["fSeekKeys"],
+            )
+        )
 
 
 def read_key(rawfile):
@@ -172,9 +337,9 @@ def read_key(rawfile):
             fSeekPdir,
         ) = _key_format_big.unpack(rawfile.read(_key_format_big.size))
 
-    fClassName = string(rawfile)
-    fName = string(rawfile)
-    fTitle = string(rawfile)
+    fClassName = read_string(rawfile)
+    fName = read_string(rawfile)
+    fTitle = read_string(rawfile)
 
     return {
         "fNbytes": fNbytes,
@@ -189,3 +354,36 @@ def read_key(rawfile):
         "fName": fName,
         "fTitle": fTitle,
     }
+
+
+def write_key(rawfile, key):
+    if key["fVersion"] <= 1000:
+        rawfile.write(
+            _key_format_small.pack(
+                key["fNbytes"],
+                key["fVersion"],
+                key["fObjlen"],
+                key["fDatime"],
+                key["fKeylen"],
+                key["fCycle"],
+                key["fSeekKey"],
+                key["fSeekPdir"],
+            )
+        )
+    else:
+        rawfile.write(
+            _key_format_big.pack(
+                key["fNbytes"],
+                key["fVersion"],
+                key["fObjlen"],
+                key["fDatime"],
+                key["fKeylen"],
+                key["fCycle"],
+                key["fSeekKey"],
+                key["fSeekPdir"],
+            )
+        )
+
+    write_string(rawfile, key["fClassName"])
+    write_string(rawfile, key["fName"])
+    write_string(rawfile, key["fTitle"])
