373dbb7e07dde01c914ffd8dcad9fa49a49fb454
Jim Pivarski <jpivarski@gmail.com>
2021-04-02 14:57:53 -0500

    Check in what I have now...

--- src/uproot/sink/file.py
+++ src/uproot/sink/file.py
@@ -19,20 +19,98 @@ class FileSink(object):
     FIXME: docstring
     """
 
+    @classmethod
+    def from_object(cls, obj):
+        """
+        FIXME: docstring
+        """
+        if (
+            callable(getattr(obj, "read", None))
+            and callable(getattr(obj, "write", None))
+            and callable(getattr(obj, "seek", None))
+            and callable(getattr(obj, "tell", None))
+            and obj.readable()
+            and obj.writable()
+            and obj.seekable()
+        ):
+            self = cls(None)
+            self._file = obj
+        else:
+            raise TypeError(
+                """writable file can only be created from a file path or an object
+
+    * that has 'read', 'write', 'seek', and 'tell' methods
+    * is 'readable() and writable() and seekable()'"""
+            )
+
     def __init__(self, file_path):
         self._file_path = file_path
         self._file = None
 
     @property
     def file_path(self):
+        """
+        FIXME: docstring
+        """
         return self._file_path
 
     def _ensure(self):
         if self._file is None:
+            if self._file_path is None:
+                raise TypeError("FileSink created from an object cannot be reopened")
             self._file = open(self._file_path, "r+b")
             self._file.seek(0)
 
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        state.pop("_file")
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__ = state
+        self._file = None
+
+    def tell(self):
+        """
+        FIXME: docstring
+        """
+        self._ensure()
+        return self._file.tell()
+
+    def flush(self):
+        """
+        FIXME: docstring
+
+        (flush is only ever user-initiated)
+        """
+        self._ensure()
+        return self._file.flush()
+
+    @property
+    def closed(self):
+        """
+        FIXME: docstring
+        """
+        return self._file is None
+
+    def close(self):
+        """
+        FIXME: docstring
+        """
+        if self._file is not None:
+            self._file.close()
+            self._file = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.close()
+
     def move_to(self, index, end=False):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         if end:
             self._file.seek(index, os.SEEK_END)
@@ -40,18 +118,44 @@ class FileSink(object):
             self._file.seek(index)
 
     def skip(self, num_bytes):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         self._file.seek(num_bytes, os.SEEK_CUR)
 
+    def read_raw(self, num_bytes):
+        """
+        FIXME: docstring
+        """
+        self._ensure()
+        return self._file.read(num_bytes)
+
+    def write_raw(self, data):
+        """
+        FIXME: docstring
+        """
+        self._ensure()
+        return self._file.write(data)
+
     def read_fields(self, format):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         return format.unpack(self._file.read(format.size))
 
     def write_fields(self, format, *fields):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         self._file.write(format.pack(fields))
 
     def read_bytestring(self):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         num_bytes = ord(self._file.read(1))
         if num_bytes == 255:
@@ -59,6 +163,9 @@ class FileSink(object):
         return self._file.read(num_bytes)
 
     def write_bytestring(self, bytestring):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         num_bytes = len(bytestring)
         if num_bytes < 255:
@@ -70,6 +177,9 @@ class FileSink(object):
 
     @staticmethod
     def bytestring_footprint(bytestring):
+        """
+        FIXME: docstring
+        """
         num_bytes = len(bytestring)
         if num_bytes < 255:
             return 1 + num_bytes
@@ -77,12 +187,18 @@ class FileSink(object):
             return 5 + num_bytes
 
     def read_string(self):
+        """
+        FIXME: docstring
+        """
         if uproot._util.py2:
             return self.read_bytestring()
         else:
             return self.read_bytestring().decode(errors="surrogateescape")
 
     def write_string(self, string):
+        """
+        FIXME: docstring
+        """
         if uproot._util.py2:
             self.write_bytestring(string)
         else:
@@ -90,9 +206,15 @@ class FileSink(object):
 
     @staticmethod
     def string_footprint(string):
+        """
+        FIXME: docstring
+        """
         return FileSink.bytestring_footprint(string.encode(errors="surrogateescape"))
 
     def read_classname(self):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         char = None
         out = []
@@ -100,9 +222,11 @@ class FileSink(object):
             char = self._file.read(1)
             if char == b"":
                 raise OSError(
-                    """C-style string has no terminator (null byte)
-in file path {0}""".format(
-                        self._file_path
+                    "C-style string has no terminator (null byte)"
+                    + (
+                        ""
+                        if self._file_path is None
+                        else "\n\nin file path " + self._file_path
                     )
                 )
             out.append(char)
@@ -113,6 +237,9 @@ in file path {0}""".format(
             return b"".join(out).decode(errors="surrogateescape")
 
     def write_classname(self, string):
+        """
+        FIXME: docstring
+        """
         self._ensure()
         if uproot._util.py2:
             bytestring = string.encode(errors="surrogateescape")
@@ -120,27 +247,3 @@ in file path {0}""".format(
             bytestring = string
         self._file.write(bytestring)
         self._file.write(b"\x00")
-
-    @property
-    def closed(self):
-        return self._file is None
-
-    def close(self):
-        if self._file is not None:
-            self._file.close()
-            self._file = None
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self.close()
-
-    def __getstate__(self):
-        state = dict(self.__dict__)
-        state.pop("_file")
-        return state
-
-    def __setstate__(self, state):
-        self.__dict__ = state
-        self._file = None
--- src/uproot/writing.py
+++ src/uproot/writing.py
@@ -6,6 +6,149 @@ FIXME: docstring
 
 from __future__ import absolute_import
 
-# import struct
+import os.path
 
-# import uproot.sink.file
+# import struct
+import uuid
+
+import uproot._util
+import uproot.compression
+import uproot.reading
+import uproot.sink.file
+
+
+def create(file_path, **options):
+    """
+    FIXME: docstring
+    """
+    file_path = uproot._util.regularize_path(file_path)
+    if uproot._util.isstr(file_path):
+        if os.path.exists(file_path):
+            raise OSError(
+                "path exists and refusing to overwrite (use 'uproot.recreate' to "
+                "overwrite)\n\nin path {0}".format(file_path)
+            )
+        file = uproot.sink.file.FileSink()
+    else:
+        file = uproot.sink.file.FileSink.from_object(file_path)
+
+    compression = options.pop("compression", update.defaults["compression"])
+    initial_bytes = options.pop("initial_bytes", create.defaults["initial_bytes"])
+    uuid_version = options.pop("uuid_version", create.defaults["uuid_version"])
+    uuid_function = options.pop("uuid", create.defaults["uuid"])
+    if len(options) != 0:
+        raise TypeError("unrecognized options: " + ", ".join(repr(x) for x in options))
+
+    _create_empty_root(file, compression, initial_bytes, uuid_version, uuid_function)
+    return WritableFile(file, compression, initial_bytes)
+
+
+create.defaults = {
+    "compression": uproot.compression.ZLIB(1),
+    "initial_bytes": 256,
+    "uuid": uuid.uuid1,
+}
+
+
+def recreate(file_path, **options):
+    """
+    FIXME: docstring
+    """
+    file_path = uproot._util.regularize_path(file_path)
+    if uproot._util.isstr(file_path):
+        file = uproot.sink.file.FileSink()
+    else:
+        file = uproot.sink.file.FileSink.from_object(file_path)
+
+    compression = options.pop("compression", update.defaults["compression"])
+    initial_bytes = options.pop("initial_bytes", recreate.defaults["initial_bytes"])
+    uuid_version = options.pop("uuid_version", create.defaults["uuid_version"])
+    uuid_function = options.pop("uuid", create.defaults["uuid"])
+    if len(options) != 0:
+        raise TypeError("unrecognized options: " + ", ".join(repr(x) for x in options))
+
+    _create_empty_root(file, compression, initial_bytes, uuid_version, uuid_function)
+    return WritableFile(file, compression, initial_bytes)
+
+
+recreate.defaults = {
+    "compression": uproot.compression.ZLIB(1),
+    "initial_bytes": 256,
+    "uuid": uuid.uuid1,
+}
+
+
+def update(file_path, **options):
+    """
+    FIXME: docstring
+    """
+    file_path = uproot._util.regularize_path(file_path)
+    if uproot._util.isstr(file_path):
+        file = uproot.sink.file.FileSink()
+    else:
+        file = uproot.sink.file.FileSink.from_object(file_path)
+
+    compression = options.pop("compression", update.defaults["compression"])
+    initial_bytes = options.pop("initial_bytes", update.defaults["initial_bytes"])
+    uuid_version = options.pop("uuid_version", create.defaults["uuid_version"])
+    uuid_function = options.pop("uuid", create.defaults["uuid"])
+    if len(options) != 0:
+        raise TypeError("unrecognized options: " + ", ".join(repr(x) for x in options))
+
+    if file.read_raw(4) != b"root":
+        _create_empty_root(
+            file, compression, initial_bytes, uuid_version, uuid_function
+        )
+    return WritableFile(file, compression, initial_bytes)
+
+
+update.defaults = {
+    "compression": uproot.compression.ZLIB(1),
+    "initial_bytes": 256,
+    "uuid": uuid.uuid1,
+}
+
+
+def _create_empty_root(file, compression, initial_bytes, uuid_version, uuid_function):
+    filename = file.file_path
+    if filename is None:
+        filename = "dynamic.root"
+    else:
+        filename = os.path.split(filename)[-1]
+    if len(filename) >= 256:
+        raise ValueError("ROOT file names must be less than 256 bytes")
+
+    # temporary
+    # streamerinfo = b"\x00\x00\x01\x19\x00\x04\x00\x00\x01ri\x04\xe0\xc5\x00@\x00\x01\x00\x00\x016\x00\x00\x00d\x05TList\x0cStreamerInfo\x12Doubly linked listZL\x08\xd0\x00\x00r\x01\x00x\x01uP;\n\xc2@\x14\x9c\x185h#v\x01\x9b\x14b\xe7\x1d\x92\x80\xa0U@\xa3X\x08\x12u\x95\x08&\xb2F\xc4K\x88\x07\xf00\xdeLg\xd7\x0fn\xe1\xc0\x9bb\xde\xcc\x0eo}X\x19*\xb0@\x94\x14\x11\x96\x0fk\xfa \xe2Q!E\xb2\x13r\x90\xadsP\rQ\xf3\x01\x97v\x1d\xb0\xc9\xf58Zl\xe9K\xb3\rn\x97\xfe'\xef\xea<W\x81\x94\xc9Ye\xab\xb0\xcd\x1a]\xc7\xe7\x8eFU\x98\x1c\x04\xed\x98\xc3&\x8fQ&w\xbe\x85\x00\x1cU(\x96E\x93\xcet\xe9\r\xa3(\xf6r\xadp\x19r\x0c\\[\xf7\xee\xafP\x0e\x83Q\x8f\x02oDa\x14\xbfo\xa0>C\x89<\xd1\xd5m\xa3z\xfd25N2\xd9\xef\xc5\xcaS\x1f\xa4\x0e\x07\x02\x8e\xcb\xf9\x0b\xe7\xe3}\x024bH."
+    # streamerinfo = b"\x00\x00\x00U\x00\x04\x00\x00\x00\x15i\x04\xe0\x9f\x00@\x00\x01\x00\x00\x00\xd8\x00\x00\x00d\x05TList\x0cStreamerInfo\x12Doubly linked list@\x00\x00\x11\x00\x05\x00\x01\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00"
+
+    # magic = b"root"
+    # fVersion = 1062206  # ROOT 6.22/06 is my model
+    # fBEGIN = 100
+    # fEND = 0  # will be replaced
+    # fSeekFree = 0  # will be replaced
+    # nfree = 0  # will be replaced
+    # fNbytesName = 0  # will be replaced
+    # fUnits = 8
+    # fCompress = compression.code
+    # fSeekInfo = 0  # will be replaced
+    # fNbytesInfo = len(streamerinfo)
+    # fUUID_version = uuid_version
+    # if callable(uuid_function):
+    #     fUUID = uuid_function()
+    # elif isinstance(uuid_function, uuid.UUID):
+    #     fUUID = uuid_function.bytes
+    # elif isinstance(uuid_function, bytes):
+    #     fUUID = uuid_function
+    # else:
+    #     fUUID = uuid.UUID(str(uuid_function)).bytes
+
+    # uproot.reading._file_header_fields_big
+
+
+class WritableFile(object):
+    """
+    FIXME: docstring
+    """
+
+    pass
