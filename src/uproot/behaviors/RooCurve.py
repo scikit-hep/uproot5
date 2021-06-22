511f2d6312e7ce02211801fc5c8da46ab691064e
Beojan Stanislaus <beojan@gmail.com>
2021-06-22 14:22:57 +0100

    Add TGraph, RooCurve, and RooHist behaviours. (#350)
    * Move RooHist.py and TGraph.py into src/upstream/..
    
    * Add full behavior implementation for RooCurve and RooHist
    
    * Add interpolate_asymm_errors to RooCurve
    
    * Fix pre-commit formatting errors.
    
    * Remove errant merge marker
    
    * Use ValueError instead of custom exception types
    
    * Missed this one
    
    * Add RooCurve and RooHist tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Rename roocurve_hist fixture to roocurve
    
    That's what's used in the test definitions
    
    * Fix mistake
    
    * Fix tests
    
    Co-authored-by: Jim Pivarski <jpivarski@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/uproot/behaviors/RooCurve.py
@@ -0,0 +1,174 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+"""
+This module defines the behaviors of ``RooCurve``.
+"""
+
+from __future__ import absolute_import
+
+import numpy
+
+import uproot
+import uproot.behaviors.TGraph
+
+# '@fUniqueID', '@fBits', 'fName', 'fTitle', 'fLineColor', 'fLineStyle', 'fLineWidth',
+# 'fFillColor', 'fFillStyle', 'fMarkerColor', 'fMarkerStyle', 'fMarkerSize', 'fNpoints',
+# 'fX', 'fY', 'fFunctions', 'fHistogram', 'fMinimum', 'fMaximum', '_yAxisLabel',
+# '_ymin', '_ymax', '_normValue'
+
+
+def _parse_errs(xvalues, errs):
+    xvals, yvals = errs.values()
+    # Index of one-past right edge
+    right_ind = numpy.argmax(xvals) + 2
+    up_x = xvals[:right_ind]
+    up_y = yvals[:right_ind]
+    down_x = numpy.flip(xvals[right_ind:])
+    down_y = numpy.flip(yvals[right_ind:])
+    if (not numpy.all(numpy.diff(up_x) >= 0)) or (
+        not numpy.all(numpy.diff(down_x) >= 0)
+    ):
+        raise ValueError("RooCurve x values are not increasing")
+    up = numpy.interp(xvalues, up_x, up_y)
+    down = numpy.interp(xvalues, down_x, down_y)
+    return (up, down)
+
+
+def _centers(edges):
+    return (edges[1:] + edges[:-1]) / 2
+
+
+class RooCurve(uproot.behaviors.TGraph.TGraph):
+    """Behaviors for RooCurve.
+
+    Beyond the behavior of a ``TGraph`` this also provides functionality to
+    interpolate the graph at provided points, or extract a stored histogram
+    (given bin edges).
+    """
+
+    @property
+    def name(self):
+        """
+        The name of the histogram.
+        """
+        return self.member("fName")
+
+    @property
+    def title(self):
+        """
+        The title of the histogram.
+        """
+        return self.member("fTitle")
+
+    @property
+    def curve_type(self):
+        """
+        Determines whether curve represents values or errors by checking if it is open or closed.
+
+        Returns "VALUES" or "ERRORS".
+        """
+        xvals = self.values(axis="x")
+        if numpy.isclose(xvals[0], xvals[-1]):
+            return "ERRORS"
+        else:
+            return "VALUES"
+
+    def interpolate(self, xvalues):
+        """
+        Args:
+            xvalues (array_like): xvalues to interpolate at.
+
+        Returns y values when RooCurve is interpolated at the given x values.
+        """
+        if self.curve_type != "VALUES":
+            raise ValueError(
+                "interpolate can only be called on a value (open) curve. "
+                "Try interpolate_errors."
+            )
+        xvals, yvals = self.values()
+        return numpy.interp(xvalues, xvals, yvals)
+
+    def interpolate_asymm_errors(self, xvalues):
+        """
+        Args:
+            xvalues (array_like): xvalues to interpolate at.
+
+        Returns:
+            up (array_like): Upper boundary of uncertainty band.
+            down (array_like): Lower boundary of uncertainty band.
+
+        Returns asymmetric y errors when RooCurve is interpolated at the given x values.
+        """
+        if self.curve_type != "ERRORS":
+            raise ValueError(
+                "interpolate_errors can only be called on an error (closed) curve. "
+                "Try interpolate."
+            )
+        up, down = _parse_errs(xvalues, self)
+        return (up, down)
+
+    def interpolate_errors(self, xvalues):
+        """
+        Args:
+            xvalues (array_like): xvalues to interpolate at.
+
+        Returns y errors when RooCurve is interpolated at the given x values.
+        """
+        if self.curve_type != "ERRORS":
+            raise ValueError(
+                "interpolate_errors can only be called on an error (closed) curve. "
+                "Try interpolate."
+            )
+        up, down = _parse_errs(xvalues, self)
+        return numpy.abs((up - down) / 2)
+
+    def to_boost(self, bin_edges, error_curve=None):
+        """
+        Args:
+            bin_edges (array_like): Bin edges for histogram.
+            error_curve (RooCurve): RooCurve visualizing errors.
+
+        Returns ``boost-histogram`` object by interpolating ``RooCurve``.
+        """
+        if self.curve_type != "VALUES":
+            raise ValueError(
+                "to_boost should be called on the value curve. The error curve is passed using the"
+                "error_curve parameter."
+            )
+        boost_histogram = uproot.extras.boost_histogram()
+        axis = boost_histogram.axis.Variable(bin_edges, underflow=False, overflow=False)
+        axis.name = self.name
+        axis.title = self.title
+        centers = _centers(bin_edges)
+        values = self.interpolate(centers)
+        if error_curve is not None:
+            errs = error_curve.interpolate_errors(centers)
+            variances = numpy.square(errs)
+            hist = boost_histogram.Histogram(
+                axis, storage=boost_histogram.storage.Weight()
+            )
+            hist.name = self.name
+            hist.title = self.title
+            view = hist.view()
+            view.value = values
+            view.variance = variances
+            return hist
+        else:
+            hist = boost_histogram.Histogram(
+                axis, storage=boost_histogram.storage.Double()
+            )
+            hist.name = self.name
+            hist.title = self.title
+            view = hist.view()
+            view[...] = values
+            return hist
+
+    def to_hist(self, bin_edges, error_curve=None):
+        """
+        Args:
+            bin_edges (array_like): Bin edges for histogram.
+            error_curve (RooCurve): RooCurve visualizing errors.
+
+        Returns ``hist`` object by interpolating ``RooCurve``.
+        """
+        return uproot.extras.hist().Hist(self.to_boost(bin_edges, error_curve))
---
+++ src/uproot/behaviors/RooHist.py
@@ -0,0 +1,102 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+"""
+This module defines the behaviors of ``RooHist``.
+"""
+
+from __future__ import absolute_import
+
+import numpy
+
+import uproot
+import uproot.behaviors.TGraphAsymmErrors
+
+# ['@fUniqueID', '@fBits', 'fName', 'fTitle', 'fLineColor', 'fLineStyle',
+#  'fLineWidth', 'fFillColor', 'fFillStyle', 'fMarkerColor', 'fMarkerStyle',
+#  'fMarkerSize', 'fNpoints', 'fX', 'fY', 'fFunctions', 'fHistogram',
+#  'fMinimum', 'fMaximum', 'fEXlow', 'fEXhigh', 'fEYlow', 'fEYhigh']
+
+
+class RooHist(uproot.behaviors.TGraphAsymmErrors.TGraphAsymmErrors):
+    """
+    Behavior for ``RooHist``.
+
+    This consists of ``to_boost`` (and ``to_hist``) and ``to_numpy``
+    providing access to the histogram.
+    """
+
+    @property
+    def name(self):
+        """
+        The name of the histogram.
+        """
+        return self.member("fName")
+
+    @property
+    def title(self):
+        """
+        The title of the histogram.
+        """
+        return self.member("fTitle")
+
+    def to_numpy(self, dd=False):
+        """
+        Args:
+            dd (bool): If True, the return type follows
+                `numpy.histogramdd <https://numpy.org/doc/stable/reference/generated/numpy.histogramdd.html>`__;
+                otherwise, it follows `numpy.histogram <https://numpy.org/doc/stable/reference/generated/numpy.histogram.html>`__
+                and `numpy.histogram2d <https://numpy.org/doc/stable/reference/generated/numpy.histogram2d.html>`__.
+
+        Converts the ``RooHist`` into a form like the ones produced by the NumPy
+        histogram functions.
+        """
+        bin_centers, values = self.values()
+        bin_edges_low = bin_centers - self.errors(which="low", axis="x")
+        bin_edges_high = bin_centers + self.errors(which="high", axis="x")
+        if not numpy.all(numpy.isclose(bin_edges_low[1:], bin_edges_high[:-1])):
+            raise ValueError("bin_edges_low[1:] != bin_edges_high[:-1]")
+        bin_edges = numpy.append(bin_edges_low, [bin_edges_high[-1]])
+        if dd:
+            return values, (bin_edges,)
+        else:
+            return values, bin_edges
+
+    def to_boost(self):
+        """
+        Converts ``RooHist`` into a ``boost-histogram`` object.
+        """
+        boost_histogram = uproot.extras.boost_histogram()
+        bin_centers, values = self.values()
+        bin_edges_low = bin_centers - self.errors(which="low", axis="x")
+        bin_edges_high = bin_centers + self.errors(which="high", axis="x")
+        # Boost histogram only supports symmetric errors
+        if not numpy.all(numpy.isclose(bin_edges_low[1:], bin_edges_high[:-1])):
+            raise ValueError("bin_edges_low[1:] != bin_edges_high[:-1]")
+        errors = self.errors(which="mean", axis="y")
+        variances = numpy.square(errors)
+
+        # Now make the Boost histogram
+        bin_edges = numpy.append(bin_edges_low, [bin_edges_high[-1]])
+        axis = boost_histogram.axis.Variable(
+            bin_edges,
+            underflow=False,
+            overflow=False,
+        )
+        axis.name = self.name
+        axis.title = self.title
+        hist = boost_histogram.Histogram(axis, storage=boost_histogram.storage.Weight())
+        hist.name = self.name
+        hist.title = self.title
+        view = hist.view()
+        view.value = values
+        view.variance = variances
+        return hist
+
+    def to_hist(self):
+        """
+        Converts ``RooHist`` into a ``hist`` object.
+        """
+        return uproot.extras.hist().Hist(self.to_boost())
+
+    def _to_boost_histogram_(self):
+        return self.to_boost()
