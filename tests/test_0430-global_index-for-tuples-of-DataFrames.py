0bc31f3729b1876814e05261698baa0584d9c207
Jim Pivarski <jpivarski@gmail.com>
2021-09-01 09:15:55 -0500

    Fix #430, global_index for tuples of DataFrames.

---
+++ tests/test_0430-global_index-for-tuples-of-DataFrames.py
@@ -0,0 +1,18 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+import numpy as np
+import pytest
+import skhep_testdata
+
+import uproot
+
+pytest.importorskip("pandas")
+
+
+def test():
+    for arrays in uproot.iterate(
+        [skhep_testdata.data_path("uproot-HZZ-uncompressed.root") + ":events"] * 2,
+        ["Muon_Px", "Jet_Px", "MET_px"],
+        library="pd",
+    ):
+        pass
