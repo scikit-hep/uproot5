deb8d88c2643521f372bf5005c51af8926016c7e
Nicholas Smith <nick.smith@cern.ch>
2021-09-17 20:58:14 -0500

    Recursively fix forms for objects that require ak.from_iter (#441)
    * Recursively fix forms for objects that require ak.from_iter
    
    * flake8

--- src/uproot/_util.py
+++ src/uproot/_util.py
@@ -664,6 +664,34 @@ def awkward_form_remove_uproot(awkward, form):
 # FIXME: Until we get Awkward reading these bytes directly, rather than
 # going through ak.from_iter, the integer dtypes will be int64 and the
 # floating dtypes will be float64 because that's what ak.from_iter makes.
+def recursively_fix_awkward_form_of_iter(awkward, interpretation, form):
+    """
+    Given an interpretation of a TBranch, fixup any form corresponding to
+    a component that cannot be read directly by awkward and needs to go
+    through ak.from_iter
+    """
+    import uproot.interpretation
+
+    if isinstance(interpretation, uproot.interpretation.grouped.AsGrouped):
+        fixed = {}
+        for key, subinterpretation in interpretation.subbranches.items():
+            fixed[key] = recursively_fix_awkward_form_of_iter(
+                awkward, subinterpretation, form.content(key)
+            )
+        return awkward.forms.RecordForm(
+            fixed,
+            form.has_identities,
+            form.parameters,
+        )
+    elif isinstance(interpretation, uproot.interpretation.objects.AsObjects):
+        if uproot.interpretation.objects.awkward_can_optimize(interpretation, form):
+            return form
+        else:
+            return uproot._util.awkward_form_of_iter(awkward, form)
+    else:
+        return form
+
+
 def awkward_form_of_iter(awkward, form):
     """
     Fix an ``ak.forms.Form`` object for a given iterable.
