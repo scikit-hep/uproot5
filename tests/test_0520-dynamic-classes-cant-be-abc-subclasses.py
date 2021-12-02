0961e3f9e5aa262f043acacfc678d7c12d7b4f7a
Jim Pivarski <jpivarski@gmail.com>
2021-12-02 15:46:28 -0600

    Only run the dynamic module test in Pythons that support it.

--- tests/test_0520-dynamic-classes-cant-be-abc-subclasses.py
+++ tests/test_0520-dynamic-classes-cant-be-abc-subclasses.py
@@ -9,6 +9,10 @@ import skhep_testdata
 import uproot
 
 
+@pytest.mark.skipif(
+    uproot._util.py26 or uproot._util.py27 or uproot._util.py35 or uproot._util.py36,
+    reason="Dynamic types depend on module __getattr__, a Python 3.7+ feature.",
+)
 def test():
     with open("tests/samples/h_dynamic.pkl", "rb") as f:
         assert len(list(pickle.load(f).axis(0))) == 100
