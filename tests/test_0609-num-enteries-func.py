7fc2b39bc208fed8fda12233279f2b166e9b9977
Kush Kothari <kushkothari2001@gmail.com>
2022-06-17 22:41:04 +0530

    Solved tests to have platform independant paths

--- tests/test_0609-num-enteries-func.py
+++ tests/test_0609-num-enteries-func.py
@@ -1,71 +1,57 @@
-import numpy
 import skhep_testdata
 
 import uproot
 
 
 def test_num_entries_single():
-    file1 = skhep_testdata.data_path("uproot-Zmumu.root") + ":events"
-    assert list(uproot.num_entries(file1)) == [
-        ("/home/kmk/.local/skhepdata/uproot-Zmumu.root", "events", 2304)
-    ]
+    file1 = skhep_testdata.data_path("uproot-Zmumu.root")
+    ttree1 = file1 + ":events"
+    assert list(uproot.num_entries(ttree1)) == [(file1, "events", 2304)]
 
 
 def test_num_entries_multiple():
-    file1 = skhep_testdata.data_path("uproot-Zmumu.root") + ":events"
-    file2 = skhep_testdata.data_path("uproot-HZZ.root") + ":events"
-    file3 = (
-        skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root") + ":sample"
-    )
+    file1 = skhep_testdata.data_path("uproot-Zmumu.root")
+    ttree1 = file1 + ":events"
+    file2 = skhep_testdata.data_path("uproot-HZZ.root")
+    ttree2 = file2 + ":events"
+    file3 = skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root")
+    ttree3 = file3 + ":sample"
 
-    assert list(uproot.num_entries([file1, file2, file3])) == [
-        ("/home/kmk/.local/skhepdata/uproot-Zmumu.root", "events", 2304),
-        ("/home/kmk/.local/skhepdata/uproot-HZZ.root", "events", 2421),
-        (
-            "/home/kmk/.local/skhepdata/uproot-sample-6.08.04-uncompressed.root",
-            "sample",
-            30,
-        ),
+    assert list(uproot.num_entries([ttree1, ttree2, ttree3])) == [
+        (file1, "events", 2304),
+        (file2, "events", 2421),
+        (file3, "sample", 30),
     ]
 
 
 def test_num_entries_as_iterator():
-    file1 = skhep_testdata.data_path("uproot-Zmumu.root") + ":events"
-    file2 = skhep_testdata.data_path("uproot-HZZ.root") + ":events"
-    file3 = (
-        skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root") + ":sample"
-    )
+    file1 = skhep_testdata.data_path("uproot-Zmumu.root")
+    ttree1 = file1 + ":events"
+    file2 = skhep_testdata.data_path("uproot-HZZ.root")
+    ttree2 = file2 + ":events"
+    file3 = skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root")
+    ttree3 = file3 + ":sample"
 
     vals = [
-        ("/home/kmk/.local/skhepdata/uproot-Zmumu.root", "events", 2304),
-        ("/home/kmk/.local/skhepdata/uproot-HZZ.root", "events", 2421),
-        (
-            "/home/kmk/.local/skhepdata/uproot-sample-6.08.04-uncompressed.root",
-            "sample",
-            30,
-        ),
+        (file1, "events", 2304),
+        (file2, "events", 2421),
+        (file3, "sample", 30),
     ]
-    for i, num in enumerate(uproot.num_entries([file1, file2, file3])):
+    for i, num in enumerate(uproot.num_entries([ttree1, ttree2, ttree3])):
         assert num == vals[i]
 
 
 def test_dict_input():
-    file_name1 = skhep_testdata.data_path("uproot-Zmumu.root")
-    file_name2 = skhep_testdata.data_path("uproot-HZZ.root")
-    file_name3 = skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root")
+    file1 = skhep_testdata.data_path("uproot-Zmumu.root")
+    file2 = skhep_testdata.data_path("uproot-HZZ.root")
+    file3 = skhep_testdata.data_path("uproot-sample-6.08.04-uncompressed.root")
 
     vals = [
-        ("/home/kmk/.local/skhepdata/uproot-Zmumu.root", "events", 2304),
-        ("/home/kmk/.local/skhepdata/uproot-HZZ.root", "events", 2421),
-        (
-            "/home/kmk/.local/skhepdata/uproot-sample-6.08.04-uncompressed.root",
-            "sample",
-            30,
-        ),
+        (file1, "events", 2304),
+        (file2, "events", 2421),
+        (file3, "sample", 30),
     ]
     for i, num in enumerate(
-        uproot.num_entries(
-            {file_name1: "events", file_name2: "events", file_name3: "sample"}
-        )
+        uproot.num_entries({file1: "events", file2: "events", file3: "sample"})
     ):
         assert num == vals[i]
