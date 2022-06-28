09f11ad26beaa540dc79ddbf39d845ac71fe3144
Moelf <proton@jling.dev>
2022-06-27 19:36:04 -0500

    day1 test

---
+++ tests/test_9999-rntuple-flat.py
@@ -0,0 +1,19 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+import json
+import queue
+import sys
+
+import numpy
+import pytest
+import skhep_testdata
+
+import uproot
+
+
+def test():
+    filename = skhep_testdata.data_path("test_ntuple_int_float.root")
+    with uproot.open(filename) as f:
+        obj = f["ntuple"]
+        assert obj.keys == ['one_integers', 'two_floats']
+        assert obj.header.field_type_names ==  ['std::int32_t', 'float']
