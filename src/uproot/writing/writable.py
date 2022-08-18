e36004ca3680da16e0bee036737e8035ff9fe0b5
Jim Pivarski <jpivarski@gmail.com>
2022-08-18 12:17:36 -0500

    These comments no longer make sense without 'noqa'.

--- src/uproot/writing/writable.py
+++ src/uproot/writing/writable.py
@@ -601,13 +601,13 @@ class WritableDirectory(MutableMapping):
         return False
 
     def __iter__(self):
-        return self.iterkeys()  # B301 (not a dict)
+        return self.iterkeys()
 
     def _ipython_key_completions_(self):
         """
         Supports key-completion in an IPython or Jupyter kernel.
         """
-        return self.iterkeys()  # (not a dict)
+        return self.iterkeys()
 
     def keys(
         self,
@@ -633,7 +633,7 @@ class WritableDirectory(MutableMapping):
         Note that this does not read any data from the file.
         """
         return list(
-            self.iterkeys(  # (not a dict)
+            self.iterkeys(
                 recursive=recursive,
                 cycle=cycle,
                 filter_name=filter_name,
@@ -664,7 +664,7 @@ class WritableDirectory(MutableMapping):
         and ``filter_classname``.
         """
         return list(
-            self.itervalues(  # (not a dict)
+            self.itervalues(
                 recursive=recursive,
                 filter_name=filter_name,
                 filter_classname=filter_classname,
@@ -696,7 +696,7 @@ class WritableDirectory(MutableMapping):
         and ``filter_classname``.
         """
         return list(
-            self.iteritems(  # (not a dict)
+            self.iteritems(
                 recursive=recursive,
                 cycle=cycle,
                 filter_name=filter_name,
@@ -771,7 +771,7 @@ class WritableDirectory(MutableMapping):
                     yield keyname
 
             if recursive and classname in ("TDirectory", "TDirectoryFile"):
-                for k1 in self._get(keyname, cyclenum).iterkeys(  # (not a dict)
+                for k1 in self._get(keyname, cyclenum).iterkeys(
                     recursive=recursive,
                     cycle=cycle,
                     filter_name=filter_name,
@@ -804,7 +804,7 @@ class WritableDirectory(MutableMapping):
         Note that this reads all objects that are selected by ``filter_name``
         and ``filter_classname``.
         """
-        for keyname in self.iterkeys(  # (not a dict)
+        for keyname in self.iterkeys(
             recursive=recursive,
             cycle=True,
             filter_name=filter_name,
@@ -836,7 +836,7 @@ class WritableDirectory(MutableMapping):
         Note that this reads all objects that are selected by ``filter_name``
         and ``filter_classname``.
         """
-        for keyname in self.iterkeys(  # (not a dict)
+        for keyname in self.iterkeys(
             recursive=recursive,
             cycle=True,
             filter_name=filter_name,
@@ -884,7 +884,7 @@ class WritableDirectory(MutableMapping):
             if recursive and classname in ("TDirectory", "TDirectoryFile"):
                 for k1, c1 in self._get(
                     keyname, cyclenum
-                ).iterclassnames(  # (not a dict)
+                ).iterclassnames(
                     recursive=recursive,
                     cycle=cycle,
                     filter_name=filter_name,
