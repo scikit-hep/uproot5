393810fb71a3020604907363f5e028049e90f42c
Moelf <proton@jling.dev>
2022-09-06 20:16:33 -0500

    tmp_path

--- tests/test_0705-rntuple-writing-metadata.py
+++ tests/test_0705-rntuple-writing-metadata.py
@@ -15,11 +15,13 @@ import os
 
 def test_flat(tmp_path):
     filepath = os.path.join(tmp_path, "test.root")
+
     with uproot.recreate(filepath) as file:
         akform = ak._v2.forms.RecordForm([ak._v2.forms.NumpyForm('float64'), 
             ak._v2.forms.NumpyForm('int32'), ak._v2.forms.NumpyForm('bool')], ['one', 'two', 'three'])
         file.mkntuple("ntuple", akform)
-    frs = uproot.open("/tmp/test.root")["ntuple"].header.field_records
+
+    frs = uproot.open(filepath)["ntuple"].header.field_records
     assert frs[0].parent_field_id == 0
     assert frs[1].parent_field_id == 1
     assert frs[2].parent_field_id == 2
@@ -30,7 +32,7 @@ def test_flat(tmp_path):
     assert frs[1].type_name == "std::int32_t"
     assert frs[2].type_name == "bit"
 
-    crs = uproot.open("/tmp/test.root")["ntuple"].header.column_records
+    crs = uproot.open(filepath)["ntuple"].header.column_records
     assert crs[0].type == 7
     assert crs[1].type == 11
     assert crs[2].type == 6
