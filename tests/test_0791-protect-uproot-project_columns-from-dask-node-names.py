1486982cf2c62f559d237e861bb2f8870f62c389
Jim Pivarski <jpivarski@gmail.com>
2023-01-06 13:12:47 -0600

    fix: an uproot.dask test was wrong; revealed by new dask-awkward.

--- tests/test_0791-protect-uproot-project_columns-from-dask-node-names.py
+++ tests/test_0791-protect-uproot-project_columns-from-dask-node-names.py
@@ -11,4 +11,12 @@ pytest.importorskip("dask_awkward")
 
 def test():
     da = uproot.dask(skhep_testdata.data_path("uproot-issue-791.root") + ":tree")
-    assert da[da.int_branch < 0].compute().tolist() == [{"int_branch": -1}]
+    assert da[da.int_branch < 0].compute().tolist() == [
+        {
+            "int_branch": -1,
+            "long_branch": -2,
+            "float_branch": 3.299999952316284,
+            "double_branch": 4.4,
+            "bool_branch": True,
+        }
+    ]
