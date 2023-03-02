9e5b1ff812287e909d6fd90836bc82fa758915e8
ioanaif <ioana.ifrim@gmail.com>
2023-03-02 11:31:34 +0100

    Re-enable failing test.

--- tests/test_0791-protect-uproot-project_columns-from-dask-node-names.py
+++ tests/test_0791-protect-uproot-project_columns-from-dask-node-names.py
@@ -1,22 +1,22 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
 
-# import numpy as np
-# import pytest
-# import skhep_testdata
+import numpy as np
+import pytest
+import skhep_testdata
 
-# import uproot
+import uproot
 
-# pytest.importorskip("dask_awkward")
+pytest.importorskip("dask_awkward")
 
 
-# def test():
-#     da = uproot.dask(skhep_testdata.data_path("uproot-issue-791.root") + ":tree")
-#     assert da[da.int_branch < 0].compute().tolist() == [
-#         {
-#             "int_branch": -1,
-#             "long_branch": -2,
-#             "float_branch": 3.299999952316284,
-#             "double_branch": 4.4,
-#             "bool_branch": True,
-#         }
-#     ]
+def test():
+    da = uproot.dask(skhep_testdata.data_path("uproot-issue-791.root") + ":tree")
+    assert da[da.int_branch < 0].compute().tolist() == [
+        {
+            "int_branch": -1,
+            "long_branch": -2,
+            "float_branch": 3.299999952316284,
+            "double_branch": 4.4,
+            "bool_branch": True,
+        }
+    ]
