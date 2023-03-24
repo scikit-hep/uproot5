5d3ad799357b7e1742fa2b3c5c9a7210d18ddfa3
ioanaif <ioana.ifrim@gmail.com>
2023-03-24 21:04:48 +0100

    Change testing to use skhep test data.

--- tests/test_852-fix-dtype-with-offsets.py
+++ tests/test_852-fix-dtype-with-offsets.py
@@ -1,14 +1,13 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
-import os
 import pytest
-import numpy as np
 import uproot
+import skhep_testdata
 
 ROOT = pytest.importorskip("ROOT")
 
 
 def test_xyz_vector_with_headers():
-    file = uproot.open("./TAtest2.root")
+    file = uproot.open(skhep_testdata.data_path("uproot-issue-513.root"))
     trkana = file["TrkAnaNeg/trkana"]
     trkana["demcent/_mom"].array()
