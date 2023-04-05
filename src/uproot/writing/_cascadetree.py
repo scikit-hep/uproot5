44c6998d09314e1ed8e22edf0d78bd513b963279
ioanaif <ioana.ifrim@gmail.com>
2023-04-05 18:09:22 +0200

    feat: have unknown type become float64 when in array context

--- src/uproot/writing/_cascadetree.py
+++ src/uproot/writing/_cascadetree.py
@@ -324,6 +324,8 @@ class Tree:
         self._key = None
 
     def _branch_ak_to_np(self, branch_datashape):
+        if type(branch_datashape).__name__ == "UnknownType":
+            return numpy.dtype("float64")
         if type(branch_datashape).__name__ == "NumpyType":
             return numpy.dtype(branch_datashape.primitive)
         elif type(branch_datashape).__name__ == "PrimitiveType":
@@ -718,7 +720,7 @@ class Tree:
                         content = content.project()
 
                     elif isinstance(content, awkward.contents.EmptyArray):
-                        content = content.to_NumpyArray()
+                        content = content.to_NumpyArray(dtype=numpy.float64)
 
                     elif isinstance(content, awkward.contents.RegularArray):
                         shape.append(content.size)
