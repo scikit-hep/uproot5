11fe5714d12c5fbf288ab4db18cc731dd8a33e65
ioanaif <ioana.ifrim@gmail.com>
2023-05-05 10:24:07 +0200

    Fix test imports.

--- tests/test_0886-fix-awkward-form-breadcrumbs.py
+++ tests/test_0886-fix-awkward-form-breadcrumbs.py
@@ -1,11 +1,6 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
-import os
-import pytest
 import uproot
-import awkward as ak
-import numpy as np
-import ROOT
 
 
 def test_fix_awkward_form_breadcrumbs():
