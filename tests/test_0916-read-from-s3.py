804aa3f864ed9e411df9a9639fa0f8ef38f6880f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-07-19 17:31:17 -0400

    add tests/test_0916-read-from-s3.py [xfail]

---
+++ tests/test_0916-read-from-s3.py
@@ -0,0 +1,24 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
+
+import pytest
+
+import uproot
+
+
+@pytest.mark.network
+def test_s3_fail():
+    with pytest.raises(Exception):
+        with uproot.source.http.S3Source(
+            "s3://pivarski-princeton/does-not-exist", timeout=0.1
+        ) as source:
+            tobytes(source.chunk(0, 100).raw_data)
+
+
+@pytest.mark.xfail
+@pytest.mark.network
+def test_read_s3():
+    with uproot.open(
+        "s3://pivarski-princeton/pythia_ppZee_run17emb.picoDst.root:PicoDst"
+    ) as f:
+        data = f["Track/Track.mPMomentum"].array(library="np")
+        assert len(data) == 100000
