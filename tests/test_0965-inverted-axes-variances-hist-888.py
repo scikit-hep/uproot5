da4291d8adefa1b34e0b9cb4eb57b2f8c9987b96
ioanaif <ioana.ifrim@gmail.com>
2023-10-03 16:30:30 +0200

    Fixes quotation mark in tests.

--- tests/test_0965-inverted-axes-variances-hist-888.py
+++ tests/test_0965-inverted-axes-variances-hist-888.py
@@ -51,6 +51,6 @@ def test_axes_variances_to_hist_3D_weighted():
 
 
 def test_users_2d_weighted_histogram():
-    with uproot.open(skhep_testdata.data_path("uproot-issue-888.root”)) as f:
+    with uproot.open(skhep_testdata.data_path("uproot-issue-888.root")) as f:
         h = f['hrecoVsgen_ll_cHel_400mttbar']
         assert (h.variances() == h.to_hist().variances()).all()
