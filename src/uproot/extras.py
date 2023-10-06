0937334fa07d958d16b466481bbbae44a4ec13bc
Angus Hollands <goosey15@gmail.com>
2023-10-06 19:40:30 +0100

    chore: update dependency warning

--- src/uproot/extras.py
+++ src/uproot/extras.py
@@ -325,11 +325,11 @@ def dask_awkward():
 or
     conda install -c conda-forge dask dask-awkward"""
         ) from err
-    if parse_version("2023.9.0") <= parse_version(dask_awkward.__version__):
+    if parse_version(dask_awkward.__version__) >= parse_version("2023.9.4"):
         return dask_awkward
     else:
         raise ModuleNotFoundError(
-            "Uproot 5.x can only be used with dask-awkward 2023.9.0 or newer; you have dask-awkward {}".format(
+            "Uproot 5.x can only be used with dask-awkward 2023.9.4 or newer; you have dask-awkward {}".format(
                 dask_awkward.__version__
             )
         )
