935ff15e5900a7621faa06aa80d93a9b73e5e790
Luis Antonio Obis Aparicio <35803280+lobis@users.noreply.github.com>
2023-10-11 09:37:35 -0500

    Update src/uproot/source/fsspec.py
    Co-authored-by: Jim Pivarski <jpivarski@users.noreply.github.com>

--- src/uproot/source/fsspec.py
+++ src/uproot/source/fsspec.py
@@ -30,7 +30,7 @@ class FSSpecSource(uproot.source.chunk.Source):
 
         self._fs, self._file_path = fsspec.core.url_to_fs(file_path, **opts)
 
-        if self._use_threads and self._fs.__class__.__name__ == "HTTPFileSystem":
+        if self._use_threads and type(self._fs).__name__ == "HTTPFileSystem":
             self._executor = concurrent.futures.ThreadPoolExecutor(
                 max_workers=self._num_workers
             )
