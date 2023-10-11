1f53cd768d8fee71fe6af97e27f329abc173daa0
ioanaif <ioana.ifrim@gmail.com>
2023-10-11 19:58:40 +0200

    Tests only the new file for now.

--- tests/test_0962-RNTuple-update.py
+++ tests/test_0962-RNTuple-update.py
@@ -7,26 +7,20 @@ import uproot
 import skhep_testdata
 
 
-def test_new_support_RNTuple_files():
+# def test_new_support_RNTuple_files():
+#     with uproot.open(
+#         "https://xrootd-local.unl.edu:1094//store/user/AGC/nanoaod-rntuple/zstd/TT_TuneCUETP8M1_13TeV-powheg-pythia8/cmsopendata2015_ttbar_19980_PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1_00000_0000.root"
+#     ) as f:
+#         obj = f["Events"]
+      
+#         array = obj.arrays(["nTau"])
+
+
+def test_new_support_RNTuple_files_2():
     with uproot.open(
-        "https://xrootd-local.unl.edu:1094//store/user/AGC/nanoaod-rntuple/zstd/TT_TuneCUETP8M1_13TeV-powheg-pythia8/cmsopendata2015_ttbar_19980_PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1_00000_0000.root"
+       "test_ntuple_int_multicluster.root"
     ) as f:
-        obj = f["Events"]
-        header_start = obj.member("fSeekHeader")
-        header_stop = header_start + obj.member("fNBytesHeader")
-
-        notifications = queue.Queue()
-
-        footer_start = obj.member("fSeekFooter")
-        footer_stop = footer_start + obj.member("fNBytesFooter")
-        header_chunk, footer_chunk = f.file.source.chunks(
-            [(header_start, header_stop), (footer_start, footer_stop)],
-            notifications,
-        )
-        # assert footer_stop - footer_start == 273
-
-        # print("FOOTER")
-        # cursor = uproot.Cursor(footer_start)
-        # cursor.debug(footer_chunk, 80)
-        # print("\n")
-        # array = obj.arrays(["nTau"])
+        obj = f["ntuple"]
+        df = obj.arrays()
+        assert df.one_integers[1] == 2
+        assert len(df.one_integers) == 100000000
