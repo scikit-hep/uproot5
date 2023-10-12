7193e411c00cb746684c05e459d213fe32e404dc
ioanaif <ioana.ifrim@gmail.com>
2023-10-12 16:16:04 +0200

    Updates testing / adds more files.

--- tests/test_0962-RNTuple-update.py
+++ tests/test_0962-RNTuple-update.py
@@ -1,24 +1,33 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
-import os
-import queue
 import pytest
 import uproot
-import skhep_testdata
+import numpy as np
 
 
-# def test_new_support_RNTuple_files():
-#     with uproot.open(
-#         "https://xrootd-local.unl.edu:1094//store/user/AGC/nanoaod-rntuple/zstd/TT_TuneCUETP8M1_13TeV-powheg-pythia8/cmsopendata2015_ttbar_19980_PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1_00000_0000.root"
-#     ) as f:
-#         obj = f["Events"]
-
-#         array = obj.arrays(["nTau"])
-
-
-def test_new_support_RNTuple_files_2():
+def test_new_support_RNTuple_split_int16_reading():
     with uproot.open("test_ntuple_int_multicluster.root") as f:
         obj = f["ntuple"]
         df = obj.arrays()
-        assert df.one_integers[1] == 2
-        assert len(df.one_integers) == 100000000
+        assert len(df.one_integers) == 1e8
+        assert df.one_integers[0] == 2
+        assert df.one_integers[-1] == 1
+        assert np.all(np.unique(df.one_integers[:len(df.one_integers)//2]) == [2])
+        assert np.all(np.unique(df.one_integers[len(df.one_integers)/2 + 1 :]) == [1])
+        
+
+def test_new_support_RNTuple_split_int32_reading():
+    with uproot.open("test_ntuple_int_5e4.root") as f:
+        obj = f["ntuple"]
+        df = obj.arrays()
+        assert len(df) == 5e4
+        assert len(df.one_integers) == 5e4
+        assert np.all(df.one_integers == np.arange(5e4)[::-1][:-1])
+
+
+def test_new_support_RNTuple_bit_bool_reading():
+    with uproot.open("test_ntuple_bit.root")as f:
+        obj = f["ntuple"]
+        df = obj.arrays()
+        assert np.all(df.one_bit == np.asarray([1, 0, 0, 1, 0, 0, 1, 0, 0, 1]))
+
