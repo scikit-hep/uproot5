7c14c69676f5158711602eb910da362b8ad9c9cc
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-10-12 11:12:40 -0500

    Revert "move "Chunk" class position in file to allow type hinting (no code changes)"
    This reverts commit d122e2040b20ec02a15e95278569e7e49be18b1c.

--- src/uproot/source/chunk.py
+++ src/uproot/source/chunk.py
@@ -36,6 +36,177 @@ class Resource:
         return self._file_path
 
 
+class Source:
+    """
+    Abstract class for physically reading and writing data from a file, which
+    might be remote.
+
+    In addition to the file handle, a :doc:`uproot.source.chunk.Source` might
+    manage a :doc:`uproot.source.futures.ResourceThreadPoolExecutor` to read
+    the file in parallel. Stopping these threads is part of the act of closing
+    the file.
+    """
+
+    def chunk(self, start, stop):
+        """
+        Args:
+            start (int): Seek position of the first byte to include.
+            stop (int): Seek position of the first byte to exclude
+                (one greater than the last byte to include).
+
+        Request a byte range of data from the file as a
+        :doc:`uproot.source.chunk.Chunk`.
+        """
+
+    def chunks(self, ranges, notifications: queue.Queue):
+        """
+        Args:
+            ranges (list of (int, int) 2-tuples): Intervals to fetch
+                as (start, stop) pairs in a single request, if possible.
+            notifications (``queue.Queue``): Indicator of completed
+                chunks. After each gets filled, it is ``put`` on the
+                queue; a listener should ``get`` from this queue
+                ``len(ranges)`` times.
+
+        Request a set of byte ranges from the file.
+
+        This method has two outputs:
+
+        * The method returns a list of unfilled
+          :doc:`uproot.source.chunk.Chunk` objects, which get filled
+          in a background thread. If you try to read data from an
+          unfilled chunk, it will wait until it is filled.
+        * The method also puts the same :doc:`uproot.source.chunk.Chunk`
+          objects onto the ``notifications`` queue as soon as they are
+          filled.
+
+        Reading data from chunks on the queue can be more efficient than
+        reading them from the returned list. The total reading time is the
+        same, but work on the filled chunks can be better parallelized if
+        it is triggered by already-filled chunks, rather than waiting for
+        chunks to be filled.
+        """
+
+    @property
+    def file_path(self):
+        """
+        A path to the file (or URL).
+        """
+        return self._file_path
+
+    @property
+    def num_bytes(self) -> int:
+        """
+        The number of bytes in the file.
+        """
+        return self._num_bytes
+
+    @property
+    def num_requests(self) -> int:
+        """
+        The number of requests that have been made (performance counter).
+        """
+        return self._num_requests
+
+    @property
+    def num_requested_chunks(self) -> int:
+        """
+        The number of :doc:`uproot.source.chunk.Chunk` objects that have been
+        requested (performance counter).
+        """
+        return self._num_requested_chunks
+
+    @property
+    def num_requested_bytes(self) -> int:
+        """
+        The number of bytes that have been requested (performance counter).
+        """
+        return self._num_requested_bytes
+
+    def close(self):
+        """
+        Manually closes the file(s) and stops any running threads.
+        """
+        self.__exit__(None, None, None)
+
+    @property
+    def closed(self) -> bool:
+        """
+        True if the associated file/connection/thread pool is closed; False
+        otherwise.
+        """
+        return self._executor.closed
+
+
+class MultithreadedSource(Source):
+    """
+    Abstract class for a :doc:`uproot.source.chunk.Source` that maintains a
+    :doc:`uproot.source.futures.ResourceThreadPoolExecutor`.
+    """
+
+    def __repr__(self):
+        path = repr(self._file_path)
+        if len(self._file_path) > 10:
+            path = repr("..." + self._file_path[-10:])
+        return "<{} {} ({} workers) at 0x{:012x}>".format(
+            type(self).__name__, path, self.num_workers, id(self)
+        )
+
+    def chunk(self, start, stop):
+        self._num_requests += 1
+        self._num_requested_chunks += 1
+        self._num_requested_bytes += stop - start
+
+        future = self.ResourceClass.future(self, start, stop)
+        chunk = Chunk(self, start, stop, future)
+        self._executor.submit(future)
+        return chunk
+
+    def chunks(self, ranges, notifications: queue.Queue):
+        self._num_requests += 1
+        self._num_requested_chunks += len(ranges)
+        self._num_requested_bytes += sum(stop - start for start, stop in ranges)
+
+        chunks = []
+        for start, stop in ranges:
+            future = self.ResourceClass.future(self, start, stop)
+            chunk = Chunk(self, start, stop, future)
+            future._set_notify(notifier(chunk, notifications))
+            self._executor.submit(future)
+            chunks.append(chunk)
+        return chunks
+
+    @property
+    def executor(self):
+        """
+        The :doc:`uproot.source.futures.ResourceThreadPoolExecutor`
+        """
+        return self._executor
+
+    @property
+    def num_workers(self):
+        """
+        The number of :doc:`uproot.source.futures.ResourceWorker` threads in
+        the :doc:`uproot.source.futures.ResourceThreadPoolExecutor`.
+        """
+        return self._executor.num_workers
+
+    @property
+    def closed(self) -> bool:
+        """
+        True if the :doc:`uproot.source.futures.ResourceThreadPoolExecutor` has
+        been shut down and the file handles have been closed.
+        """
+        return self._executor.closed
+
+    def __enter__(self):
+        self._executor.__enter__()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self._executor.__exit__(exception_type, exception_value, traceback)
+
+
 class Chunk:
     """
     Args:
@@ -279,177 +450,6 @@ outside expected range {self._start}:{self._stop} for this Chunk""",
                 context,
                 self._source.file_path,
             )
-
-
-class Source:
-    """
-    Abstract class for physically reading and writing data from a file, which
-    might be remote.
-
-    In addition to the file handle, a :doc:`uproot.source.chunk.Source` might
-    manage a :doc:`uproot.source.futures.ResourceThreadPoolExecutor` to read
-    the file in parallel. Stopping these threads is part of the act of closing
-    the file.
-    """
-
-    def chunk(self, start, stop) -> Chunk:
-        """
-        Args:
-            start (int): Seek position of the first byte to include.
-            stop (int): Seek position of the first byte to exclude
-                (one greater than the last byte to include).
-
-        Request a byte range of data from the file as a
-        :doc:`uproot.source.chunk.Chunk`.
-        """
-
-    def chunks(self, ranges, notifications: queue.Queue):
-        """
-        Args:
-            ranges (list of (int, int) 2-tuples): Intervals to fetch
-                as (start, stop) pairs in a single request, if possible.
-            notifications (``queue.Queue``): Indicator of completed
-                chunks. After each gets filled, it is ``put`` on the
-                queue; a listener should ``get`` from this queue
-                ``len(ranges)`` times.
-
-        Request a set of byte ranges from the file.
-
-        This method has two outputs:
-
-        * The method returns a list of unfilled
-          :doc:`uproot.source.chunk.Chunk` objects, which get filled
-          in a background thread. If you try to read data from an
-          unfilled chunk, it will wait until it is filled.
-        * The method also puts the same :doc:`uproot.source.chunk.Chunk`
-          objects onto the ``notifications`` queue as soon as they are
-          filled.
-
-        Reading data from chunks on the queue can be more efficient than
-        reading them from the returned list. The total reading time is the
-        same, but work on the filled chunks can be better parallelized if
-        it is triggered by already-filled chunks, rather than waiting for
-        chunks to be filled.
-        """
-
-    @property
-    def file_path(self):
-        """
-        A path to the file (or URL).
-        """
-        return self._file_path
-
-    @property
-    def num_bytes(self) -> int:
-        """
-        The number of bytes in the file.
-        """
-        return self._num_bytes
-
-    @property
-    def num_requests(self) -> int:
-        """
-        The number of requests that have been made (performance counter).
-        """
-        return self._num_requests
-
-    @property
-    def num_requested_chunks(self) -> int:
-        """
-        The number of :doc:`uproot.source.chunk.Chunk` objects that have been
-        requested (performance counter).
-        """
-        return self._num_requested_chunks
-
-    @property
-    def num_requested_bytes(self) -> int:
-        """
-        The number of bytes that have been requested (performance counter).
-        """
-        return self._num_requested_bytes
-
-    def close(self):
-        """
-        Manually closes the file(s) and stops any running threads.
-        """
-        self.__exit__(None, None, None)
-
-    @property
-    def closed(self) -> bool:
-        """
-        True if the associated file/connection/thread pool is closed; False
-        otherwise.
-        """
-        return self._executor.closed
-
-
-class MultithreadedSource(Source):
-    """
-    Abstract class for a :doc:`uproot.source.chunk.Source` that maintains a
-    :doc:`uproot.source.futures.ResourceThreadPoolExecutor`.
-    """
-
-    def __repr__(self):
-        path = repr(self._file_path)
-        if len(self._file_path) > 10:
-            path = repr("..." + self._file_path[-10:])
-        return "<{} {} ({} workers) at 0x{:012x}>".format(
-            type(self).__name__, path, self.num_workers, id(self)
-        )
-
-    def chunk(self, start, stop) -> Chunk:
-        self._num_requests += 1
-        self._num_requested_chunks += 1
-        self._num_requested_bytes += stop - start
-
-        future = self.ResourceClass.future(self, start, stop)
-        chunk = Chunk(self, start, stop, future)
-        self._executor.submit(future)
-        return chunk
-
-    def chunks(self, ranges, notifications: queue.Queue):
-        self._num_requests += 1
-        self._num_requested_chunks += len(ranges)
-        self._num_requested_bytes += sum(stop - start for start, stop in ranges)
-
-        chunks = []
-        for start, stop in ranges:
-            future = self.ResourceClass.future(self, start, stop)
-            chunk = Chunk(self, start, stop, future)
-            future._set_notify(notifier(chunk, notifications))
-            self._executor.submit(future)
-            chunks.append(chunk)
-        return chunks
-
-    @property
-    def executor(self):
-        """
-        The :doc:`uproot.source.futures.ResourceThreadPoolExecutor`
-        """
-        return self._executor
-
-    @property
-    def num_workers(self) -> int:
-        """
-        The number of :doc:`uproot.source.futures.ResourceWorker` threads in
-        the :doc:`uproot.source.futures.ResourceThreadPoolExecutor`.
-        """
-        return self._executor.num_workers
-
-    @property
-    def closed(self) -> bool:
-        """
-        True if the :doc:`uproot.source.futures.ResourceThreadPoolExecutor` has
-        been shut down and the file handles have been closed.
-        """
-        return self._executor.closed
-
-    def __enter__(self):
-        self._executor.__enter__()
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self._executor.__exit__(exception_type, exception_value, traceback)
 
 
 def notifier(chunk: Chunk, notifications: queue.Queue):
