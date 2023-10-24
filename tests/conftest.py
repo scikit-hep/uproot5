36e52974837c13af0849ffdd25653b44510affd2
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-10-24 16:35:57 -0500

    pytest file server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
 import pytest
+import threading
+import contextlib
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import skhep_testdata
 
 import uproot
 
@@ -9,3 +14,25 @@ import uproot
 def reset_classes():
     uproot.model.reset_classes()
     return
+
+
+@contextlib.contextmanager
+def serve():
+    port = 24211
+    server_address = ("", port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    os.chdir(skhep_testdata.local_files._cache_path())
+    th = threading.Thread(target=httpd.serve_forever, daemon=True)
+    th.start()
+    try:
+        yield f"http://127.0.0.1:{port}"
+    finally:
+        httpd.socket.close()
+        httpd.shutdown()
+        th.join()
+
+
+@pytest.fixture(scope="module")
+def server():
+    with serve() as server:
+        yield server
