c222b29e6ee81a2525f3dc7a12342482d2115883
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-10-25 15:53:33 -0500

    attempt to close properly

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@
 import pytest
 import threading
 import contextlib
-import os
 import skhep_testdata
+from functools import partial
 
 # The base http server does not support range requests. Watch https://github.com/python/cpython/issues/86809 for updates
 from http.server import HTTPServer
@@ -22,26 +22,26 @@ def reset_classes():
 @contextlib.contextmanager
 def serve():
     # serve files from the skhep_testdata data directory
-    files_directory = skhep_testdata.local_files._cache_path()
-    os.chdir(files_directory)
+    handler = partial(
+        RangeRequestHandler, directory=skhep_testdata.local_files._cache_path()
+    )
+    server = HTTPServer(server_address=("localhost", 0), RequestHandlerClass=handler)
+    server.server_activate()
 
-    server = None
-    thread = None
+    def serve_forever(httpd=server):
+        with httpd:
+            httpd.serve_forever()
+
+    thread = threading.Thread(target=serve_forever, daemon=True)
+
     try:
-        server = HTTPServer(
-            server_address=("", 0), RequestHandlerClass=RangeRequestHandler
-        )
-        thread = threading.Thread(target=server.serve_forever, daemon=True)
         thread.start()
-        yield f"http://127.0.0.1:{server.server_port}"
+        address, port = server.server_address
+        yield f"http://{address}:{port}"
     finally:
-        try:
-            if server:
-                server.server_close()
-            if thread:
-                thread.join()
-        except Exception as e:
-            print("Exception while shutting down the local http server", e)
+        # stop the server
+        server.shutdown()
+        thread.join()
 
 
 @pytest.fixture(scope="module")
