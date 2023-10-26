3341ce06ed271825697d66c2c255c3a990350aa8
Luis Antonio Obis Aparicio <35803280+lobis@users.noreply.github.com>
2023-10-26 11:58:47 -0500

    test: local http server for tests (#1010)
    * pytest file server
    
    * range request handler
    
    * use local server on some tests
    
    * skip format
    
    * variable name
    
    * use local http server
    
    * fix issue with parametrize indirect
    
    * remove network marks
    
    * attempt to fix tests
    
    * attempt to fix tests
    
    * attempt to fix ci
    
    * attempt to properly close
    
    * work on windows please
    
    * attempt to close properly
    
    * Revert "attempt to fix tests"
    
    This reverts commit 5d721dfcbe31ad595e40994c399769cbbb25d5ac.
    
    * Revert "attempt to fix tests"
    
    This reverts commit 5254c24a6fa6ddc6ad33a61ec28f2d75866ac797.
    
    * update fixture scope
    
    * do not run problematic test in local server
    
    * rename test
    
    * rename tests
    
    * reenabled example.com tests
    
    * download all files (only once)
    
    * download files on demand

--- tests/test_0692_fsspec.py
+++ tests/test_0692_fsspec.py
@@ -8,13 +8,14 @@ import skhep_testdata
 import queue
 
 
-@pytest.mark.network
 @pytest.mark.parametrize("use_threads", [True, False])
-def test_open_fsspec_http(use_threads):
+def test_open_fsspec_http(server, use_threads):
     pytest.importorskip("aiohttp")
 
+    url = f"{server}/uproot-issue121.root"
+
     with uproot.open(
-        "https://github.com/scikit-hep/scikit-hep-testdata/raw/v0.4.33/src/skhep_testdata/data/uproot-issue121.root",
+        url,
         handler=uproot.source.fsspec.FSSpecSource,
         use_threads=use_threads,
     ) as f:
@@ -23,10 +24,10 @@ def test_open_fsspec_http(use_threads):
 
 
 @pytest.mark.network
+@pytest.mark.skip(
+    reason="skipping due to GitHub API rate limitations - this should work fine - see https://github.com/scikit-hep/uproot5/pull/973 for details"
+)
 def test_open_fsspec_github():
-    pytest.skip(
-        "skipping due to GitHub API rate limitations - this should work fine - see https://github.com/scikit-hep/uproot5/pull/973 for details"
-    )
     with uproot.open(
         "github://scikit-hep:scikit-hep-testdata@v0.4.33/src/skhep_testdata/data/uproot-issue121.root",
         handler=uproot.source.fsspec.FSSpecSource,
@@ -86,11 +87,10 @@ def test_open_fsspec_xrootd(handler, use_threads):
         assert (data == 194778).all()
 
 
-@pytest.mark.network
-def test_fsspec_chunks():
+def test_fsspec_chunks(server):
     pytest.importorskip("aiohttp")
 
-    url = "https://github.com/scikit-hep/scikit-hep-testdata/raw/v0.4.33/src/skhep_testdata/data/uproot-issue121.root"
+    url = f"{server}/uproot-issue121.root"
 
     notifications = queue.Queue()
     with uproot.source.fsspec.FSSpecSource(url) as source:
