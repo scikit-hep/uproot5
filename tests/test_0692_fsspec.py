c31c888ea2312a43f8d5f8f91ae3a7fa972a74c4
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-11-03 12:05:37 -0500

    zip/tar tests

--- tests/test_0692_fsspec.py
+++ tests/test_0692_fsspec.py
@@ -170,17 +170,14 @@ def test_fsspec_tar(tmp_path):
     with open(skhep_testdata.data_path("uproot-issue121.root"), "rb") as f:
         contents = f.read()
 
-    # write the file to a compressed archive
-    filename_tar = f"{tmp_path}/uproot-issue121.tar"
-    with tarfile.open(filename_tar, "w") as tar:
+    filename_tar = f"{tmp_path}/uproot-issue121.root.tar"
+    with tarfile.open(filename_tar, mode="w") as tar:
         file_info = tarfile.TarInfo(name=filename)
         file_info.size = len(contents)
         tar.addfile(file_info, fileobj=io.BytesIO(contents))
 
     # open with fsspec
-    with uproot.open(
-        f"tar://{filename_tar}!{filename}", handler=uproot.source.fsspec.FSSpecSource
-    ) as f:
+    with uproot.open(f"tar://{filename_tar}") as f:
         data = f["Events/MET_pt"].array(library="np")
         assert len(data) == 40
 
@@ -192,14 +189,11 @@ def test_fsspec_zip(tmp_path):
     with open(skhep_testdata.data_path("uproot-issue121.root"), "rb") as f:
         contents = f.read()
 
-    # write the file to a compressed archive
-    filename_zip = f"{tmp_path}/uproot-issue121.zip"
-    with zipfile.ZipFile(filename_zip, "w") as zip_file:
-        zip_file.writestr(filename, contents)
+    filename_zip = f"{tmp_path}/uproot-issue121.root.zip"
+    with zipfile.ZipFile(filename_zip, mode="w") as zip_file:
+        zip_file.writestr(filename, data=contents)
 
     # open with fsspec
-    with uproot.open(
-        f"zip://{filename_zip}", handler=uproot.source.fsspec.FSSpecSource
-    ) as f:
+    with uproot.open(f"zip://{filename_zip}") as f:
         data = f["Events/MET_pt"].array(library="np")
         assert len(data) == 40
