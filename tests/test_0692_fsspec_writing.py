28b0f7bdbeb18b54ae2af4740607d982eea51b35
Luis Antonio Obis Aparicio <35803280+lobis@users.noreply.github.com>
2023-11-15 14:44:50 -0600

    feat: basic fsspec writing (#1016)
    * use paramiko instead of sshfs
    
    * use specified port
    
    * test default handler behaviour
    
    * default to fsspec instead of error if scheme not found
    
    * attempt to close socket
    
    * fix ci
    
    * Revert "fix ci"
    
    This reverts commit e56e337d24227073a98f73353e1dbd77a7744caf.
    
    * broader exception
    
    * also handle socket exception
    
    * get user robust
    
    * enable github test with skip if api limit is hit (so we sometimes test it, but it never fails due to api limits). TODO: update exception class
    
    * add memory filesystem test
    
    * add zip and tar tests
    
    * fix memory test
    
    * zip/tar tests
    
    * add test for writing with fsspec without any integration (file-like object)
    
    * update docstrings, add some type hints
    
    * add helper method to produce sink from a path or file-like object
    
    * add fsspec writing test for integration
    
    * helper function to check if file-like object
    
    * return file-like object
    
    * explicit argument
    
    * rename test
    
    * create empty file
    
    * rename test
    
    * pass storage options to fsspec
    
    * test ssh and memory writing
    
    * split fsspec tests into reading and writing
    
    * loosen the check for file-like object
    
    * add test for file update
    
    * file update test
    
    * properly truncate the file
    
    * refactor how sink is created (no changes)
    
    * annotation to avoid warning
    
    * close file if sink initialization fails
    
    * sink will handle fsspec in a similar way to local path for open and close
    
    * rename to reading
    
    * check if test works
    
    * missing import
    
    * remove parent dirs
    
    * fix zip tar tests
    
    * skip github if api limits hit
    
    * attempt to fix windows paths
    
    * use more complex uri with object in zip test
    
    * debug
    
    * paths
    
    * add new test case to object url split
    
    * add new failing test case: TODO make it work
    
    * revert debug changes
    
    * working in new test case
    
    * modified where file is truncated
    
    * revert file-like check
    
    * revert is-file-like check
    
    * unified rb+ mode (fsspec uses "rb+" not "r+b" it can be confusing sometimes)
    
    * add http write test (not implemented error check)
    
    * skip test for debugging
    
    * understand test failure
    
    * use r+b instead of rb+
    
    * use new sink
    
    * writing memory test
    
    * correctly truncate file with fsspec
    
    * use fsspec to get parent dir
    
    * aiohttp import skip
    
    * isstr
    
    * use ports instead of string
    
    * improve path:obj split to handle chained protocols (protocol1::protocol2://)
    
    * working url chain to some extent
    
    * attempt to fix ci
    
    * cleanup
    
    * attempt to fix ci

---
+++ tests/test_0692_fsspec_writing.py
@@ -0,0 +1,125 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/main/LICENSE
+
+import pytest
+import uproot
+import uproot.source.fsspec
+
+import os
+import fsspec
+import numpy as np
+
+
+def test_fsspec_writing_no_integration(tmp_path):
+    uri = os.path.join(tmp_path, "some", "path", "file.root")
+    with fsspec.open(uri, mode="wb") as file_obj:
+        # write a simple root file
+        with uproot.recreate(file_obj) as f:
+            f["tree"] = {"x": np.array([1, 2, 3])}
+
+    with uproot.open(uri) as f:
+        assert f["tree"]["x"].array().tolist() == [1, 2, 3]
+
+
+@pytest.mark.parametrize("scheme", ["", "file://", "simplecache::file://"])
+def test_fsspec_writing_local(tmp_path, scheme):
+    uri = scheme + os.path.join(tmp_path, "some", "path", "file.root")
+    with uproot.recreate(uri) as f:
+        f["tree"] = {"x": np.array([1, 2, 3])}
+
+    with uproot.open(uri) as f:
+        assert f["tree"]["x"].array().tolist() == [1, 2, 3]
+
+
+def test_fsspec_writing_http(server):
+    pytest.importorskip("aiohttp")
+
+    uri = f"{server}/file.root"
+
+    with pytest.raises(NotImplementedError):
+        # TODO: review this when fsspec supports writing to http
+        with uproot.recreate(uri) as f:
+            f["tree"] = {"x": np.array([1, 2, 3])}
+
+        with uproot.open(uri) as f:
+            assert f["tree"]["x"].array().tolist() == [1, 2, 3]
+
+
+@pytest.mark.parametrize(
+    "scheme",
+    [
+        "",
+        "file://",
+        "simplecache::file://",
+    ],
+)
+def test_fsspec_writing_local_update(tmp_path, scheme):
+    uri = scheme + os.path.join(tmp_path, "some", "path", "file.root")
+    with uproot.recreate(uri) as f:
+        f["tree1"] = {"x": np.array([1, 2, 3])}
+
+    with uproot.update(uri) as f:
+        f["tree2"] = {"y": np.array([4, 5, 6])}
+
+    # read data and compare
+    with uproot.open(uri) as f:
+        assert f["tree1"]["x"].array().tolist() == [1, 2, 3]
+        assert f["tree2"]["y"].array().tolist() == [4, 5, 6]
+
+
+@pytest.mark.parametrize(
+    "scheme",
+    [
+        "ssh://",
+        "simplecache::ssh://",
+    ],
+)
+def test_fsspec_writing_ssh(tmp_path, scheme):
+    pytest.importorskip("paramiko")
+    import paramiko
+    import getpass
+
+    user = getpass.getuser()
+    host = "localhost"
+    port = 22
+
+    # only test this if we can connect to the host (this will work in GitHub Actions)
+    try:
+        with paramiko.SSHClient() as client:
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(hostname=host, port=port, username=user)
+    except (
+        paramiko.ssh_exception.SSHException,
+        paramiko.ssh_exception.NoValidConnectionsError,
+    ) as e:
+        pytest.skip(f"ssh connection to host failed: {e}")
+
+    local_path = os.path.join(tmp_path, "file.root")
+    uri = f"{scheme}{user}@{host}:{port}{local_path}"
+
+    with uproot.recreate(uri) as f:
+        f["tree"] = {"x": np.array([1, 2, 3])}
+
+    with uproot.open(uri) as f:
+        assert f["tree"]["x"].array().tolist() == [1, 2, 3]
+
+
+@pytest.mark.parametrize(
+    "scheme",
+    [
+        "memory://",
+        "simplecache::memory://",
+    ],
+)
+def test_fsspec_writing_memory(tmp_path, scheme):
+    uri = f"{scheme}{tmp_path}/file.root"
+
+    # when https://github.com/fsspec/filesystem_spec/pull/1426 is merged this will work, we must remove the catch
+    with pytest.raises(ValueError):
+        with uproot.recreate(uri) as f:
+            f["tree"] = {"x": np.array([1, 2, 3])}
+
+        with uproot.open(uri) as f:
+            assert f["tree"]["x"].array().tolist() == [1, 2, 3]
+
+        if scheme == "simplecache::memory://":
+            raise ValueError("wait for next fsspec release and remove this")
