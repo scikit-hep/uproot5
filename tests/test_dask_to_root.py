660e92335088b959c3e29c0ab66813957b0c5b7a
Zoë Bilodeau <zobilodeau@gmail.com>
2024-01-19 11:02:20 +0100

    switched dependency again

--- tests/test_dask_to_root.py
+++ tests/test_dask_to_root.py
@@ -1,6 +1,7 @@
 import pytest
-import uproot
 import awkward as ak
+import fsspec
+import uproot
 
 skhep_testdata = pytest.importorskip("skhep_testdata")
 dask = pytest.importorskip("dask")
@@ -8,48 +9,37 @@ da = pytest.importorskip("dask.array")
 dask_awkward = pytest.importorskip("dask_awkward")
 math = pytest.importorskip("math")
 
-import fsspec
 
 fs = fsspec.filesystem("file")
-tmpdir = "src/uproot/my-output"
-files = fs.ls(tmpdir)
+TMPDIR = "src/uproot/my-output"
+files = fs.ls(TMPDIR)
 
 
 def simple_test(partitions):
-    a = ak.Array([{"a": [1, 2, 3]}, {"a": [4, 5]}])
-    d = dask_awkward.from_awkward(a, npartitions=partitions)
-    uproot.dask_write(d, tmpdir, prefix="data")
-    f = uproot.open(
+    arr = ak.Array([{"a": [1, 2, 3]}, {"a": [4, 5]}])
+    dask_arr = dask_awkward.from_awkward(arr, npartitions=partitions)
+    uproot.dask_write(dask_arr, TMPDIR, prefix="data")
+    file_1 = uproot.open(
         "/Users/zobil/Documents/uproot5/src/uproot/my-output/data-part0.root"
     )
-    assert len(f["tree"]["a"].arrays()) == math.ceil(len(a) / partitions)
-    assert ak.all(f["tree"]["a"].arrays()["a"][0] == a[0]["a"])
-    f1 = uproot.open(
+    assert len(file_1["tree"]["a"].arrays()) == math.ceil(len(arr) / partitions)
+    assert ak.all(file_1["tree"]["a"].arrays()["a"][0] == arr[0]["a"])
+    file_2 = uproot.open(
         "/Users/zobil/Documents/uproot5/src/uproot/my-output/data-part1.root"
     )
-    assert len(f1["tree"]["a"].arrays()) == int(len(a) / partitions)
-    assert ak.all(f1["tree"]["a"].arrays()["a"][0] == a[1]["a"])
+    assert len(file_2["tree"]["a"].arrays()) == int(len(arr) / partitions)
+    assert ak.all(file_2["tree"]["a"].arrays()["a"][0] == arr[1]["a"])
 
 
 def HZZ_test(partitions):
-    a = uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))["events"].arrays()
-    d = dask_awkward.from_awkward(ak.from_iter(a), partitions)
-    uproot.dask_write(d, tmpdir, prefix="data")
-    f = uproot.open(
+    arr = uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))["events"].arrays()
+    dask_arr = dask_awkward.from_awkward(ak.from_iter(arr), partitions)
+    uproot.dask_write(dask_arr, TMPDIR, prefix="data")
+    file_1 = uproot.open(
         "/Users/zobil/Documents/uproot5/src/uproot/my-output/data-part0.root"
     )
-    assert len(f["tree"]["Jet_Px"].arrays()) == math.ceil(len(a) / partitions)
-    f1 = uproot.open(
+    assert len(file_1["tree"]["Jet_Px"].arrays()) == math.ceil(len(arr) / partitions)
+    file_2 = uproot.open(
         "/Users/zobil/Documents/uproot5/src/uproot/my-output/data-part1.root"
     )
-    assert len(f1["tree"]["Jet_Px"].arrays()) == int(len(a) / partitions)
-
-
-simple_test(2)
-
-import os
-
-assert [os.path.basename(_) for _ in sorted(files)] == [
-    "data-part0.root",
-    "data-part1.root",
-]
+    assert len(file_2["tree"]["Jet_Px"].arrays()) == int(len(arr) / partitions)
