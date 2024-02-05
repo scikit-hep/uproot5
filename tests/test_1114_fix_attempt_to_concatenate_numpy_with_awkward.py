65659bee894b3736eaa314680300e2e8775a8be8
ioanaif <ioana.ifrim@gmail.com>
2024-02-05 15:07:01 +0100

    Removes remote file test.

--- tests/test_1114_fix_attempt_to_concatenate_numpy_with_awkward.py
+++ tests/test_1114_fix_attempt_to_concatenate_numpy_with_awkward.py
@@ -27,11 +27,3 @@ def test_partially_fix_issue_951():
                 branch.interpretation._forth = True
                 branch.array()
 
-
-def test_fix_issue_1101():
-
-    with uproot.open(
-        "root://xcache.af.uchicago.edu//root://xrootd.echo.stfc.ac.uk:1094/atlas:datadisk/rucio/mc23_13p6TeV/26/c7/DAOD_PHYSLITE.35010028._000001.pool.root.1"
-    ) as f:
-        data = f["CollectionTree"]["TruthBottomAuxDyn.childLinks"]
-        data.array()
