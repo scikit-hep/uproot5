879ac48190c3b4451b8827dc9ac6e627ed104a83
ioanaif <ioana.ifrim@gmail.com>
2024-02-06 10:58:59 +0100

    Rewrite the test

--- tests/test_1114_fix_attempt_to_concatenate_numpy_with_awkward.py
+++ tests/test_1114_fix_attempt_to_concatenate_numpy_with_awkward.py
@@ -7,22 +7,13 @@ import skhep_testdata
 
 def test_partially_fix_issue_951():
 
-    with uproot.open(
-        skhep_testdata.data_path("uproot-issue-951.root") + ":CollectionTree"
-    ) as tree:
-
+    with uproot.open(skhep_testdata.data_path("uproot-issue-951.root") + ":CollectionTree") as tree:
         for key, branch in tree.iteritems(filter_typename="*ElementLink*"):
-
-            # ignore for now the two branches which have different issues (basket 0 has the wrong number of bytes) and (uproot.interpretation.identify.UnknownInterpretation: none of the rules matched)
-            if (
-                key == "METAssoc_AnalysisMETAux./METAssoc_AnalysisMETAux.jetLink"
-                or "EventInfoAuxDyn.hardScatterVertexLink/EventInfoAuxDyn.hardScatterVertexLink"
-            ):
-                pass
-
-            else:
-                branch.interpretation._forth = False
-                branch.array()
-
+            try:
                 branch.interpretation._forth = True
                 branch.array()
+            except TypeError as e:
+                raise e
+            except:
+                # ignore for now the two branches which have different issues (basket 0 has the wrong number of bytes) and (uproot.interpretation.identify.UnknownInterpretation: none of the rules matched)
+                pass
