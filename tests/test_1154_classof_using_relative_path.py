027cabba38f4807c6a29ba42ff0c46fb94bc0f0e
ioanaif <ioana.ifrim@gmail.com>
2024-03-01 11:16:19 +0100

    Test for colon and slash in path

--- tests/test_1154_classof_using_relative_path.py
+++ tests/test_1154_classof_using_relative_path.py
@@ -1,6 +1,7 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
 import uproot
+import skhep_testdata
 import numpy as np
 
 
@@ -19,3 +20,12 @@ def test_descend_into_path_classname_of():
     with uproot.open("https://scikit-hep.org/uproot3/examples/nesteddirs.root") as g:
         assert g.classname_of("one/two/tree") == "TTree"
         assert g.classname_of("one/two/tree/Int64") == "TBranch"
+
+
+    # check both colon and slash
+    with uproot.open(
+        skhep_testdata.data_path("uproot-small-evnt-tree-fullsplit.root")
+    ) as f:
+        f.classname_of("tree:evt") == "TBranch"
+        f.classname_of("tree/evt") == "TBranch"
+
