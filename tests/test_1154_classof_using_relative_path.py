0c223651eaf1c32df42411bb235801f675c39fa8
Jim Pivarski <jpivarski@gmail.com>
2024-03-05 10:27:41 -0600

    use 'tmp_path' to prevent temporary files from becoming permanent

--- tests/test_1154_classof_using_relative_path.py
+++ tests/test_1154_classof_using_relative_path.py
@@ -1,16 +1,19 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 
+import os
+
 import uproot
 import skhep_testdata
 import numpy as np
 
 
-def test_descend_into_path_classname_of():
+def test_descend_into_path_classname_of(tmp_path):
+    filename = os.path.join(tmp_path, "test.root")
 
-    with uproot.recreate("test.root") as f:
+    with uproot.recreate(filename) as f:
         f["Tree"] = {"x": np.array([1, 2, 3, 4, 5])}
 
-    with uproot.open("test.root") as f:
+    with uproot.open(filename) as f:
         assert f.classname_of("Tree/x") == "TBranch"
         assert f.title_of("Tree/x").startswith("x/")
         assert f.class_of("Tree/x") == uproot.models.TBranch.Model_TBranch
