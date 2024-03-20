cdaf9a7a352d476f60e42e63df57ce90e1b0c3eb
Bo Johnson <bo.johnson5@icloud.com>
2024-03-20 15:54:32 -0400

    feat: add `__array__` method to ROOTRVec, STLVector, and STLSet (#1172)
    * add numpy.asarray method to STLVector and STLSet
    
    * also ROOTRVec
    
    * for STLSet, it's called `self._keys`, not `self._values`
    
    ---------
    
    Co-authored-by: Johnson, Bo <johnsonba@ornl.gov>
    Co-authored-by: Jim Pivarski <jpivarski@users.noreply.github.com>

--- src/uproot/containers.py
+++ src/uproot/containers.py
@@ -1496,6 +1496,9 @@ class ROOTRVec(Container, Sequence):
         else:
             return False
 
+    def __array__(self, *args, **kwargs):
+        return numpy.asarray(self._vector, *args, **kwargs)
+
     def tolist(self):
         return [
             x.tolist() if isinstance(x, (Container, numpy.ndarray)) else x for x in self
@@ -1551,6 +1554,9 @@ class STLVector(Container, Sequence):
             return self._values == other
         else:
             return False
+
+    def __array__(self, *args, **kwargs):
+        return numpy.asarray(self._vector, *args, **kwargs)
 
     def tolist(self):
         return [
@@ -1616,6 +1622,9 @@ class STLSet(Container, Set):
             return keys_same
         else:
             return numpy.all(keys_same)
+
+    def __array__(self, *args, **kwargs):
+        return numpy.asarray(self._keys, *args, **kwargs)
 
     def tolist(self):
         return {
