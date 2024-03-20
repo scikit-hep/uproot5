cc69876140c4183a1c66a0443879ac09f06aedf3
Jim Pivarski <jpivarski@gmail.com>
2024-03-20 12:56:20 -0500

    give up on any kind of leading slash for Windows

--- tests/test_0692_fsspec_writing.py
+++ tests/test_0692_fsspec_writing.py
@@ -49,7 +49,7 @@ def test_fsspec_writing_local(tmp_path, scheme):
 )
 @pytest.mark.parametrize(
     "slash_prefix",
-    ["", ("\\" if is_windows else "/")],
+    [""] if is_windows else ["", "/"],
 )
 def test_fsspec_writing_local_uri(tmp_path, scheme, slash_prefix, filename):
     uri = scheme + slash_prefix + os.path.join(tmp_path, "some", "path", filename)
