099018a0974b7dbdd621e6271d9afae548b7b332
Andres Rios Tascon <ariostas@gmail.com>
2024-10-09 14:02:30 -0400

    Added test for #1285

---
+++ tests/test_1285_rntuple_multicluster_concatenation.py
@@ -0,0 +1,23 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
+
+import skhep_testdata
+import numpy as np
+
+import uproot
+
+
+def test_schema_extension():
+    filename = skhep_testdata.data_path("test_ntuple_index_multicluster.root")
+    with uproot.open(filename) as f:
+        obj = f["ntuple"]
+
+        arrays = obj.arrays()
+        int_vec_array = arrays["int_vector"]
+
+        true_array = np.zeros((200, 2), dtype=np.int32)
+        true_array[:100, 0] = np.arange(100, dtype=np.int32)
+        true_array[:100, 1] = np.arange(100, dtype=np.int32)
+        true_array[100:, 0] = np.arange(100, dtype=np.int32)
+        true_array[100:, 1] = np.arange(100, dtype=np.int32) + 1
+
+        assert np.array_equal(int_vec_array, true_array)
