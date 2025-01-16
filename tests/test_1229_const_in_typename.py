e1357aab43f53bfcfeb293289bb61b55094d4da5
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-01-16 15:28:56 -0500

    add back the file-based test (since it was added to skhep_testdata)

--- tests/test_1229_const_in_typename.py
+++ tests/test_1229_const_in_typename.py
@@ -3,8 +3,25 @@
 import pytest
 import skhep_testdata
 
+import uproot
 from uproot.interpretation.identify import parse_typename
 
 
-def test_const_in_typename():
+@pytest.fixture(scope="module")
+def datafile(tmpdir_factory):
+    yield skhep_testdata.data_path("uproot-issue-1229.root")
+
+
+@pytest.fixture
+def tree(datafile):
+    with uproot.open(datafile) as f:
+        yield f["tree"]
+
+
+def test_const_in_typename(tree):
+    assert tree["branch/pointer"].typename == 'TFooMember*'
+    assert tree["branch/const_pointer"].typename == 'TFooMember*'
+
+
+def test_const_parse_typename():
     assert parse_typename("TH1I*") == parse_typename("const TH1I*")
