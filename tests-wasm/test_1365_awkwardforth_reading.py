c9ed677a9898f3c8f95d3b73828621807c64274b
Andres Rios Tascon <ariostas@gmail.com>
2025-07-10 11:38:45 -0700

    Change awkwardforth test for pyodide

--- tests-wasm/test_1365_awkwardforth_reading.py
+++ tests-wasm/test_1365_awkwardforth_reading.py
@@ -1,150 +1,23 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 from __future__ import annotations
 
+import pytest
 from utils import run_test_in_pyodide
 
 
-# Taken from test_1347_rntuple_floats_suppressed_cols.py
-@run_test_in_pyodide(test_file="test_float_types_rntuple_v1-0-0-0.root")
-def test_rntuple_with_awkwardforth(selenium):
-    import numpy as np
-
+# Taken from test_0637_setup_tests_for_AwkwardForth.py
+@run_test_in_pyodide(test_file="issue367b.root")
+@pytest.mark.parametrize("is_forth", [False, True])
+def test_00(is_forth):
     import uproot
 
-    def truncate_float(value, bits):
-        a = np.float32(value).view(np.uint32)
-        a &= np.uint32(0xFFFFFFFF) << (32 - bits)
-        return a.astype(np.uint32).view(np.float32)
-
-    def quantize_float(value, bits, min, max):
-        min = np.float32(min)
-        max = np.float32(max)
-        if value < min or value > max:
-            raise ValueError(f"Value {value} is out of range [{min}, {max}]")
-        scaled_value = (value - min) * (2**bits - 1) / (max - min)
-        int_value = np.round(scaled_value)
-        quantized_float = min + int_value * (max - min) / ((1 << bits) - 1)
-        return quantized_float.astype(np.float32)
-
-    with uproot.open("test_float_types_rntuple_v1-0-0-0.root") as f:
-        obj = f["ntuple"]
-
-        arrays = obj.arrays()
-
-        min_value = -2.0
-        max_value = 3.0
-
-        entry = arrays[0]
-        true_value = 1.23456789
-        assert entry.trunc10 == truncate_float(true_value, 10)
-        assert entry.trunc16 == truncate_float(true_value, 16)
-        assert entry.trunc24 == truncate_float(true_value, 24)
-        assert entry.trunc31 == truncate_float(true_value, 31)
-        assert np.isclose(
-            entry.quant1, quantize_float(true_value, 1, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant8, quantize_float(true_value, 8, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant16, quantize_float(true_value, 16, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant20, quantize_float(true_value, 20, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant24, quantize_float(true_value, 24, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant25, quantize_float(true_value, 25, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant32, quantize_float(true_value, 32, min_value, max_value)
-        )
-
-        entry = arrays[1]
-        true_value = 1.4660155e13
-        assert entry.trunc10 == truncate_float(true_value, 10)
-        assert entry.trunc16 == truncate_float(true_value, 16)
-        assert entry.trunc24 == truncate_float(true_value, 24)
-        assert entry.trunc31 == truncate_float(true_value, 31)
-        true_value = 1.6666666
-        assert np.isclose(
-            entry.quant1, quantize_float(true_value, 1, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant8, quantize_float(true_value, 8, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant16, quantize_float(true_value, 16, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant20, quantize_float(true_value, 20, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant24, quantize_float(true_value, 24, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant25, quantize_float(true_value, 25, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant32, quantize_float(true_value, 32, min_value, max_value)
-        )
-
-        entry = arrays[2]
-        true_value = -6.2875986e-22
-        assert entry.trunc10 == truncate_float(true_value, 10)
-        assert entry.trunc16 == truncate_float(true_value, 16)
-        assert entry.trunc24 == truncate_float(true_value, 24)
-        assert entry.trunc31 == truncate_float(true_value, 31)
-        assert np.isclose(
-            entry.quant1, quantize_float(true_value, 1, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant8, quantize_float(true_value, 8, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant16, quantize_float(true_value, 16, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant20, quantize_float(true_value, 20, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant24, quantize_float(true_value, 24, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant25,
-            quantize_float(true_value, 25, min_value, max_value),
-            atol=2e-07,
-        )
-        assert np.isclose(
-            entry.quant32, quantize_float(true_value, 32, min_value, max_value)
-        )
-
-        entry = arrays[3]
-        true_value = -1.9060668
-        assert entry.trunc10 == truncate_float(true_value, 10)
-        assert entry.trunc16 == truncate_float(true_value, 16)
-        assert entry.trunc24 == truncate_float(true_value, 24)
-        assert entry.trunc31 == truncate_float(true_value, 31)
-        assert np.isclose(
-            entry.quant1, quantize_float(true_value, 1, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant8, quantize_float(true_value, 8, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant16, quantize_float(true_value, 16, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant20, quantize_float(true_value, 20, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant24, quantize_float(true_value, 24, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant25, quantize_float(true_value, 25, min_value, max_value)
-        )
-        assert np.isclose(
-            entry.quant32, quantize_float(true_value, 32, min_value, max_value)
-        )
+    with uproot.open("issue367b.root") as file:
+        branch = file["tree/weights"]
+        interp = uproot.interpretation.identify.interpretation_of(branch, {}, False)
+        interp._forth = is_forth
+        py = branch.array(interp, library="ak", entry_stop=2)
+        assert py[0]["0"][0] == "expskin_FluxUnisim"
+        # py[-1] == <STLMap {'expskin_FluxUnisim': [0.944759093019904, 1.0890682745548674, ..., 1.1035170311451232, 0.8873957186284592], ...} at 0x7fbc4c1325e0>
+        assert py.layout.form == interp.awkward_form(branch.file)
+        if is_forth:
+            assert interp._complete_forth_code is not None
