88b421f29cc02e36e3c12cdbf97d73355447e2da
Andres Rios Tascon <ariostas@gmail.com>
2025-07-10 11:48:56 -0700

    Changed test again

--- tests-wasm/test_1365_awkwardforth_reading.py
+++ tests-wasm/test_1365_awkwardforth_reading.py
@@ -5,19 +5,16 @@ import pytest
 from utils import run_test_in_pyodide
 
 
-# Taken from test_0637_setup_tests_for_AwkwardForth.py
-@run_test_in_pyodide(test_file="issue367b.root")
-@pytest.mark.parametrize("is_forth", [False, True])
-def test_00(is_forth):
+# Taken from test_0610_awkward_form.py
+@run_test_in_pyodide(test_file="uproot-HZZ-objects.root")
+def test_awkward_array_tvector2_array_forth():
     import uproot
 
-    with uproot.open("issue367b.root") as file:
-        branch = file["tree/weights"]
-        interp = uproot.interpretation.identify.interpretation_of(branch, {}, False)
-        interp._forth = is_forth
-        py = branch.array(interp, library="ak", entry_stop=2)
-        assert py[0]["0"][0] == "expskin_FluxUnisim"
-        # py[-1] == <STLMap {'expskin_FluxUnisim': [0.944759093019904, 1.0890682745548674, ..., 1.1035170311451232, 0.8873957186284592], ...} at 0x7fbc4c1325e0>
-        assert py.layout.form == interp.awkward_form(branch.file)
-        if is_forth:
-            assert interp._complete_forth_code is not None
+    awk_data = None
+    with uproot.open("uproot-HZZ-objects.root")["events/MET"] as tree:
+        interp = uproot.interpretation.identify.interpretation_of(tree, {}, False)
+        interp._forth = True
+        awk_data = tree.array(interp, library="ak")
+    assert awk_data[0]["fX"] == pytest.approx(5.912771224975586)
+    assert awk_data[4]["fY"] == pytest.approx(-1.3100523948669434)
+    assert awk_data[1200]["fX"] == pytest.approx(1.9457910060882568)
