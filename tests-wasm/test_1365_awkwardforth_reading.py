889857b2b303b7bc6b782555caed6fe8bbe39919
Andres Rios Tascon <ariostas@gmail.com>
2025-07-11 10:21:12 -0700

    Fixed test

--- tests-wasm/test_1365_awkwardforth_reading.py
+++ tests-wasm/test_1365_awkwardforth_reading.py
@@ -1,13 +1,14 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
 from __future__ import annotations
 
-import pytest
 from utils import run_test_in_pyodide
 
 
 # Taken from test_0610_awkward_form.py
-@run_test_in_pyodide(test_file="uproot-HZZ-objects.root")
+@run_test_in_pyodide(packages=["numpy"], test_file="uproot-HZZ-objects.root")
 def test_awkward_array_tvector2_array_forth(selenium):
+    import numpy as np
+
     import uproot
 
     awk_data = None
@@ -15,6 +16,6 @@ def test_awkward_array_tvector2_array_forth(selenium):
         interp = uproot.interpretation.identify.interpretation_of(tree, {}, False)
         interp._forth = True
         awk_data = tree.array(interp, library="ak")
-    assert awk_data[0]["fX"] == pytest.approx(5.912771224975586)
-    assert awk_data[4]["fY"] == pytest.approx(-1.3100523948669434)
-    assert awk_data[1200]["fX"] == pytest.approx(1.9457910060882568)
+    assert np.isclose(awk_data[0]["fX"], 5.912771224975586)
+    assert np.isclose(awk_data[4]["fY"], -1.3100523948669434)
+    assert np.isclose(awk_data[1200]["fX"], 1.9457910060882568)
