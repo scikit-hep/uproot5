4ea9dcbd9cec106f9d1ac0d016e1efcc6eeaf5f5
mrli <mrli@ihep.ac.cn>
2025-07-30 23:39:45 +0800

    Only keep `CustomInterpretation` as base class

--- src/uproot/interpretation/custom.py
+++ src/uproot/interpretation/custom.py
@@ -16,6 +16,7 @@ import uproot.extras
 import uproot.interpretation
 
 awkward = uproot.extras.awkward()
+pandas = uproot.extras.pandas()
 
 
 class CustomInterpretation(uproot.interpretation.Interpretation):
@@ -107,67 +108,21 @@ class CustomInterpretation(uproot.interpretation.Interpretation):
         arr_to_concat = [
             basket_arrays[i] for i in range(basket_start_idx, basket_end_idx + 1)
         ]
-        tot_array = awkward.concatenate(arr_to_concat)
 
         relative_entry_start = entry_start - basket_entry_starts[basket_start_idx]
         relative_entry_stop = entry_stop - basket_entry_starts[basket_start_idx]
 
-        return tot_array[relative_entry_start:relative_entry_stop]
-
-
-class AsBinary(uproot.interpretation.Interpretation):
-    """
-    Return binary data of the ``TBasket``. Pass an instance of this class
-    to :ref:`uproot.behaviors.TBranch.TBranch.array` like this:
-
-    .. code-block:: python
-        binary_data = branch.array(interpretation=AsBinary())
-
-    """
-
-    @property
-    def cache_key(self) -> str:
-        return id(self)
-
-    def basket_array(
-        self,
-        data,
-        byte_offsets,
-        basket,
-        branch,
-        context,
-        cursor_offset,
-        library,
-        interp_options,
-    ):
-        counts = byte_offsets[1:] - byte_offsets[:-1]
-        awkward = uproot.extras.awkward()
-        return awkward.unflatten(data, counts)
-
-    def final_array(
-        self,
-        basket_arrays,
-        entry_start,
-        entry_stop,
-        entry_offsets,
-        library,
-        branch,
-        options,
-    ):
-        basket_entry_starts = numpy.array(entry_offsets[:-1])
-        basket_entry_stops = numpy.array(entry_offsets[1:])
-
-        basket_start_idx = numpy.where(basket_entry_starts <= entry_start)[0].max()
-        basket_end_idx = numpy.where(basket_entry_stops >= entry_stop)[0].min()
-
-        arr_to_concat = [
-            basket_arrays[i] for i in range(basket_start_idx, basket_end_idx + 1)
-        ]
-
-        awkward = uproot.extras.awkward()
-        tot_array = awkward.concatenate(arr_to_concat)
-
-        relative_entry_start = entry_start - basket_entry_starts[basket_start_idx]
-        relative_entry_stop = entry_stop - basket_entry_starts[basket_start_idx]
-
-        return tot_array[relative_entry_start:relative_entry_stop]
+        if isinstance(arr_to_concat, awkward.Array):
+            tot_array = awkward.concatenate(arr_to_concat)
+            return tot_array[relative_entry_start:relative_entry_stop]
+        elif isinstance(arr_to_concat, numpy.ndarray):
+            tot_array = numpy.concatenate(arr_to_concat)
+            return tot_array[relative_entry_start:relative_entry_stop]
+        elif isinstance(arr_to_concat, pandas.DataFrame):
+            tot_array = pandas.concat(arr_to_concat, ignore_index=True)
+            return tot_array.iloc[relative_entry_start:relative_entry_stop]
+        else:
+            raise TypeError(
+                f"Unsupported array type: {type(arr_to_concat)}. "
+                "Expected an Awkward Array, NumPy array, or Pandas DataFrame."
+            )
