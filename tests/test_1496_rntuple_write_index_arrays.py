1f769b61d152d3a3fb527864674c9ba417c2efad
Andres Rios Tascon <ariostas@gmail.com>
2025-09-10 10:10:41 -0400

    Test with 32-bit indices

--- tests/test_1496_rntuple_write_index_arrays.py
+++ tests/test_1496_rntuple_write_index_arrays.py
@@ -11,25 +11,41 @@ ak = pytest.importorskip("awkward")
 record_array = ak.Array(
     [{"x": 1, "y": 2}, {"x": 3, "y": 4}, {"x": 5, "y": 6}, {"x": 7, "y": 8}]
 ).layout
-record_index_array = ak.contents.IndexedArray(
-    ak.index.Index([1, 0, 2, 2]),
+record_index64_array = ak.contents.IndexedArray(
+    ak.index.Index64([1, 0, 2, 2]),
     record_array,
 )
-record_index_option_array = ak.contents.IndexedOptionArray(
-    ak.index.Index([1, -1, 2, 2]),
+record_index32_array = ak.contents.IndexedArray(
+    ak.index.Index32([1, 0, 2, 2]),
     record_array,
 )
-record_list_array = ak.contents.ListArray(
-    ak.index.Index([1, 0, 2, 2]),
-    ak.index.Index([4, 4, 4, 4]),
+record_index64_option_array = ak.contents.IndexedOptionArray(
+    ak.index.Index64([1, -1, 2, 2]),
+    record_array,
+)
+record_index32_option_array = ak.contents.IndexedOptionArray(
+    ak.index.Index32([1, -1, 2, 2]),
+    record_array,
+)
+record_list64_array = ak.contents.ListArray(
+    ak.index.Index64([1, 0, 2, 2]),
+    ak.index.Index64([4, 4, 4, 4]),
+    record_array,
+)
+record_list32_array = ak.contents.ListArray(
+    ak.index.Index32([1, 0, 2, 2]),
+    ak.index.Index32([4, 4, 4, 4]),
     record_array,
 )
 
 data = ak.Array(
     {
-        "record_index_array": record_index_array,
-        "record_index_option_array": record_index_option_array,
-        "record_list_array": record_list_array,
+        "record_index64_array": record_index64_array,
+        "record_index32_array": record_index32_array,
+        "record_index64_option_array": record_index64_option_array,
+        "record_index32_option_array": record_index32_option_array,
+        "record_list64_array": record_list64_array,
+        "record_list32_array": record_list32_array,
     }
 )
 
@@ -46,7 +62,7 @@ def test_writing_and_reading(tmp_path):
     arrays = obj.arrays()
 
     for f in data.fields:
-        if f == "record_index_option_array":
+        if f in ("record_index64_option_array", "record_index32_option_array"):
             assert [
                 t[0].tolist() if len(t) > 0 else None for t in arrays[f][:4]
             ] == data[f].tolist()
