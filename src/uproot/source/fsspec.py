0ea97db05b96459bc2793961e6b6c4a354f782e9
Ianna Osborne <ianna.osborne@cern.ch>
2025-10-06 17:30:13 +0200

    wrap cernbox urls with simplecache::

--- src/uproot/source/fsspec.py
+++ src/uproot/source/fsspec.py
@@ -33,6 +33,15 @@ class FSSpecSource(uproot.source.chunk.Source):
     ):
         super().__init__()
         self._coalesce_config = coalesce_config
+        
+        # Wrap CERNBox URLs automatically with simplecache::
+        def _maybe_wrap_cernbox(url: str) -> str:
+            if "cernbox.cern.ch/remote.php/dav/public-files/" in url:
+                return f"simplecache::{url}"
+            return url
+
+        file_path = _maybe_wrap_cernbox(file_path)
+        
         self._fs, self._file_path = fsspec.core.url_to_fs(
             file_path, **self.extract_fsspec_options(options)
         )
