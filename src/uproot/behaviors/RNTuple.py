cc7440a1ceb0074eb80df3852e43aaea7adc131e
Rachit931 <symbolused@gmail.com>
2026-01-31 01:50:50 +0530

    Validate RNTuple offset array legnth during array construction

--- src/uproot/behaviors/RNTuple.py
+++ src/uproot/behaviors/RNTuple.py
@@ -841,16 +841,15 @@ class HasFields(Mapping):
         )
         entry_start -= cluster_offset
         entry_stop -= cluster_offset
-
-        if entry_start == 0 and entry_stop == cluster_num_entries:
-            _validate_rntuple_offsets(container_dict, cluster_num_entries)
-
         arrays = uproot.extras.awkward().from_buffers(
             form,
             cluster_num_entries,
             container_dict,
             backend="cuda" if interpreter == "gpu" and backend == "cuda" else "cpu",
         )[entry_start:entry_stop]
+
+        if entry_start == 0 and entry_stop == cluster_num_entries:
+            _validate_rntuple_offsets(container_dict, cluster_num_entries)
 
         arrays = uproot.extras.awkward().to_backend(arrays, backend=backend)
         # no longer needed; save memory
