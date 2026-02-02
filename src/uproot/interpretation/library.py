44146b1fac149670db70087efa093e60fc388533
Rachit931 <symbolused@gmail.com>
2026-02-02 22:26:55 +0530

    Update ix unsafe equality comparison for interpretation libraries

--- src/uproot/interpretation/library.py
+++ src/uproot/interpretation/library.py
@@ -167,8 +167,9 @@ class Library:
     def __eq__(self, other):
         if not isinstance(other, Library):
             return NotImplemented
-        
+
         return type(_libraries[self.name]) is type(_libraries[other.name])
+
 
 class NumPy(Library):
     """
