0784ebca762f1e06e7e004916a78a980a2337f2a
Andres Rios Tascon <ariostas@gmail.com>
2026-03-16 15:11:00 -0400

    Add test for mkrntuple

--- tests/test_1599_mktree_mkrntuple_with_subdir.py
+++ tests/test_1599_mktree_mkrntuple_with_subdir.py
@@ -30,3 +30,30 @@ def test_mktree_repeated_same_subdir(tmp_path):
             "tree/z",
             "tree/w",
         }
+
+
+def test_mkrntuple_repeated_same_subdir(tmp_path):
+    newfile = os.path.join(tmp_path, "newfile.root")
+
+    with uproot.recreate(newfile) as f:
+        f.mkrntuple("ntpl/x", {"x": [1, 2, 3]})
+        f.mkrntuple("ntpl/y", {"x": [1, 2, 3]})
+        f.mkrntuple("ntpl/z", {"x": [1, 2, 3]})
+        f.mkrntuple("ntpl/w", {"x": [1, 2, 3]})
+
+        assert set(f.keys(cycle=False)) == {
+            "ntpl",
+            "ntpl/x",
+            "ntpl/y",
+            "ntpl/z",
+            "ntpl/w",
+        }
+
+    with uproot.open(newfile) as fin:
+        assert set(fin.keys(cycle=False)) == {
+            "ntpl",
+            "ntpl/x",
+            "ntpl/y",
+            "ntpl/z",
+            "ntpl/w",
+        }
