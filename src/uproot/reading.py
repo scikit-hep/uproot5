bf60166750ae4da2130e2f9bfebc3d2020f6445d
tomeichlersmith <eichl008@umn.edu>
2026-08-12 14:28:06 -0500

    finish description of uproot.open options

--- src/uproot/reading.py
+++ src/uproot/reading.py
@@ -82,15 +82,26 @@ def open(
     Options (type; default):
 
     * handler (:doc:`uproot.source.chunk.Source` class; None)
-    * timeout (float for HTTP, int for XRootD, ignored for other sources; 30)
+        Class implementing reading from the data source.
+        If None, deduced from input file type.
+    * timeout (float for HTTP, int for XRootD; default defined by source implementation)
         The time in seconds to wait before giving up on the connection.
+        Ignored for non-internet sources like local file paths.
     * max_num_elements (None or int; None)
-        The maximum number of elements to be requested in a single vector read, when using XRootD.
+       The maximum number of elements to be requested in a single vector read, when using XRootD.
     * num_workers (int; 1)
+        Number of tasks to spawn for reading, only used by some source types
     * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True)
+        Use multi-threading when spawning workers.
     * num_fallback_workers (int; 10)
+        Number of tasks to spawn for reading in fallback mode (for example, multi-threading
+        requests instead of a multipart GET for an http source)
     * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be)
+        Size of first chunk that we attempt to read in bytes.
     * minimal_ttree_metadata (bool; True)
+        Skip rarely used metadata and defer reading of embedded TBaskets
+    * http_max_header_bytes (int; 21784)
+        Maximum size of HTTP packet in bytes when the source is http
 
     Any object derived from a ROOT file is a context manager (works in Python's
     ``with`` statement) that closes the file when exiting the ``with`` block.
@@ -539,13 +550,14 @@ class ReadOnlyFile(CommonFileMethods):
     * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True)
         Use multi-threading when spawning workers.
     * num_fallback_workers (int; 10)
-        ?
+        Number of tasks to spawn for reading in fallback mode (for example, multi-threading
+        requests instead of a multipart GET for an http source)
     * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be)
         Size of first chunk that we attempt to read in bytes.
     * minimal_ttree_metadata (bool; True)
         Skip rarely used metadata and defer reading of embedded TBaskets
-    * http_max_header_bytes
-        ?
+    * http_max_header_bytes (int; 21784)
+        Maximum size of HTTP packet in bytes when the source is http
 
     See the `ROOT TFile documentation <https://root.cern.ch/doc/master/classTFile.html>`__
     for a specification of ``TFile`` header fields.
