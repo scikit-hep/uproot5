cdafd5a143474aa4318d0695bc1eb426c3f8500f
Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
2026-08-19 15:37:09 -0500

    docs: add descriptions of reading and writing options (#1702)
    * add descriptions of options in uproot.open
    
    * quick updates from concrete comments
    
    * finish description of uproot.open options
    
    * share rst documenting reading/writing options
    
    * style: pre-commit fixes
    
    * fixup formatting
    
    * style: pre-commit fixes
    
    * forgot space between dots and include
    
    * copy options doc into docstrings so they are visible from help
    
    add them to behavrios/RNTuple and specialize uproot.update to point out which options are ignored
    
    * style: pre-commit fixes
    
    * ignore all files named _build no matter where they are
    
    * remove options that raise exceptions for writing.update
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/uproot/behaviors/RNTuple.py
+++ src/uproot/behaviors/RNTuple.py
@@ -140,17 +140,28 @@ def iterate(
     * already-open RNTuple objects.
     * iterables of the above.
 
-    Options (type; default): (Not implemented yet.)
+    ..
+      Future Dev Note: These shared reading options are manually copied across multiple files
+      so they appear in `help` output of Python interpreter and the online documentation
+      for the functions where they are used.
 
-    * handler (:doc:`uproot.source.chunk.Source` class; None)
-    * timeout (float for HTTP, int for XRootD; 30)
-    * max_num_elements (None or int; None)
-        The maximum number of elements to be requested in a single vector read, when using XRootD.
-    * num_workers (int; 1)
-    * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True)
-    * num_fallback_workers (int; 10)
-    * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be)
-    * minimal_ttree_metadata (bool; True)
+    Options (type; default):
+
+    * handler (:doc:`uproot.source.chunk.Source` class; None): Class implementing reading from the data source.
+      If None, deduced from input file type.
+    * timeout (float for HTTP, int for XRootD; default defined by source implementation): The time in seconds
+      to wait before giving up on the connection. Ignored for non-internet sources like local file paths.
+    * max_num_elements (None or int; None): The maximum number of elements to be requested in a single vector
+      read, when using XRootD.
+    * num_workers (int; 1): Number of tasks to spawn for reading, only used by some source types
+    * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True):
+      Use multi-threading when spawning workers.
+    * num_fallback_workers (int; 10): Number of tasks to spawn for reading in fallback mode
+      (for example, multi-threading requests instead of a multipart GET for an http source)
+    * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be): Size of first chunk that we attempt
+      to read in bytes.
+    * minimal_ttree_metadata (bool; True): Skip rarely used metadata and defer reading of embedded TBaskets
+    * http_max_header_bytes (int; 21784): Maximum size of HTTP packet in bytes when the source is http
 
     See also :ref:`uproot.behaviors.RNTuple.HasFields.iterate` to iterate
     within a single file.
@@ -333,17 +344,28 @@ def concatenate(
     * already-open RNTuple objects.
     * iterables of the above.
 
-    Options (type; default): (Not implemented yet.)
+    ..
+      Future Dev Note: These shared reading options are manually copied across multiple files
+      so they appear in `help` output of Python interpreter and the online documentation
+      for the functions where they are used.
 
-    * handler (:doc:`uproot.source.chunk.Source` class; None)
-    * timeout (float for HTTP, int for XRootD; 30)
-    * max_num_elements (None or int; None)
-        The maximum number of elements to be requested in a single vector read, when using XRootD.
-    * num_workers (int; 1)
-    * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True)
-    * num_fallback_workers (int; 10)
-    * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be)
-    * minimal_ttree_metadata (bool; True)
+    Options (type; default):
+
+    * handler (:doc:`uproot.source.chunk.Source` class; None): Class implementing reading from the data source.
+      If None, deduced from input file type.
+    * timeout (float for HTTP, int for XRootD; default defined by source implementation): The time in seconds
+      to wait before giving up on the connection. Ignored for non-internet sources like local file paths.
+    * max_num_elements (None or int; None): The maximum number of elements to be requested in a single vector
+      read, when using XRootD.
+    * num_workers (int; 1): Number of tasks to spawn for reading, only used by some source types
+    * use_threads (bool; False on the emscripten platform (i.e. in a web browser), else True):
+      Use multi-threading when spawning workers.
+    * num_fallback_workers (int; 10): Number of tasks to spawn for reading in fallback mode
+      (for example, multi-threading requests instead of a multipart GET for an http source)
+    * begin_chunk_size (memory_size; 403, the smallest a ROOT file can be): Size of first chunk that we attempt
+      to read in bytes.
+    * minimal_ttree_metadata (bool; True): Skip rarely used metadata and defer reading of embedded TBaskets
+    * http_max_header_bytes (int; 21784): Maximum size of HTTP packet in bytes when the source is http
 
     Other file entry points:
 
